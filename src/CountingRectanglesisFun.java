e2d5aff438d6010637c11acba26e79360961c3e8
bhishmaraj <matib275@gmail.com>
2017-05-17 12:03:29 +0530

    commit at 17/05/2017 12:03:29 IST

---
+++ src/CountingRectanglesisFun.java
@@ -0,0 +1,98 @@
+import java.util.*;
+import java.io.*;
+public class CountingRectanglesisFun {
+    
+    
+    
+    /************************ SOLUTION STARTS HERE ************************/
+    
+    
+    private static void solve() {
+        
+        
+        int N = nextInt();
+        int M = nextInt();
+        int Q = nextInt();
+        
+        char arr[][] = new char[N][];
+        for(int i = 0; i < N; i++)
+            arr[i] = nextLine().toCharArray();
+        
+        int DP[][][][] = new int[N][M][N][M];
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                
+                if(arr[i][j] == '0')
+                    DP[i][j][i][j] = 1;
+                
+                // Base case 
+                for(int ii = i + 1; ii < N; ii++) 
+                    if(arr[ii][j] == '0' && DP[i][j][ii - 1][j] > 0)
+                        DP[i][j][ii][j] = DP[i][j][ii - 1][j] + 1;
+                    
+                for(int jj = j + 1; jj < M; jj++)
+                    if(arr[i][jj] == '0' && DP[i][j][i][jj - 1] > 0)
+                        DP[i][j][i][jj] = DP[i][j][i][jj - 1] + 1;
+                
+                for(int ii = i + 1; ii < N; ii++)
+                    for(int jj = j + 1; jj < M; jj++)
+                        if(DP[i][j][ii][jj - 1] > 0 && DP[i][j][ii - 1][j] > 0)
+                            DP[i][j][ii][jj] = DP[i][j][ii][jj - 1] + DP[i][j][ii - 1][jj] 
+                                               - DP[i][j][ii - 1][jj - 1] + 1;
+            }
+        }
+        
+        while(Q-->0) {
+            int a = nextInt() - 1;
+            int b = nextInt() - 1;
+            int c = nextInt() - 1;
+            int d = nextInt() - 1;
+            int cnt = 0;
+            for(int i = a; i <= c; i++)
+                for(int j = b; j <= d; j++)
+                    cnt += DP[i][j][c][d];
+            println(cnt);
+        }
+    }
+    
+    
+    
+    /************************ SOLUTION ENDS HERE ************************/
+    
+    
+    
+    
+    
+    /************************ TEMPLATE STARTS HERE **********************/
+    
+    public static void main(String[] args) throws IOException {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)), false);
+        st     = null;
+        solve();
+        reader.close();
+        writer.close();
+    }
+    
+    static BufferedReader reader;
+    static PrintWriter    writer;
+    static StringTokenizer st;
+    
+    static String next()
+    {while(st == null || !st.hasMoreTokens()){try{String line = reader.readLine();if(line == null){return null;}            
+    st = new StringTokenizer(line);}catch (Exception e){throw new RuntimeException();}}return st.nextToken();}
+    static String nextLine()  {String s=null;try{s=reader.readLine();}catch(IOException e){e.printStackTrace();}return s;}             
+    static int    nextInt()   {return Integer.parseInt(next());}
+    static long   nextLong()  {return Long.parseLong(next());}     
+    static double nextDouble(){return Double.parseDouble(next());}
+    static char   nextChar()  {return next().charAt(0);}
+    static int[]  nextIntArray(int n)         {int[] a= new int[n];   int i=0;while(i<n){a[i++]=nextInt();}  return a;}
+    static long[] nextLongArray(int n)        {long[]a= new long[n];  int i=0;while(i<n){a[i++]=nextLong();} return a;}    
+    static int[]  nextIntArrayOneBased(int n) {int[] a= new int[n+1]; int i=1;while(i<=n){a[i++]=nextInt();} return a;}            
+    static long[] nextLongArrayOneBased(int n){long[]a= new long[n+1];int i=1;while(i<=n){a[i++]=nextLong();}return a;}            
+    static void   print(Object o)  { writer.print(o);  }
+    static void   println(Object o){ writer.println(o);}
+    
+    /************************ TEMPLATE ENDS HERE ************************/
+    
+}
