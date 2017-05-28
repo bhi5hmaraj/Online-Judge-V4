8f7463abe295a5e3014122d45bc131edbcf7b07b
bhishmaraj <matib275@gmail.com>
2017-05-28 18:58:01 +0530

    commit at 28/05/2017 18:58:01 IST

---
+++ src/CONSESNK.java
@@ -0,0 +1,79 @@
+import java.util.*;
+import java.io.*;
+class CONSESNK {
+    
+    
+    
+    /************************ SOLUTION STARTS HERE ************************/
+    
+    
+    private static void solve() {
+        
+        int T = nextInt();
+        while(T-->0) {
+            
+            int N = nextInt();
+            long L = nextLong();
+            long A = nextLong();
+            long B = nextLong();
+            
+            long arr[] = nextLongArray(N);
+            Arrays.sort(arr);
+            long optX;
+            long maxX = B - (L * N);
+            if(arr[0] < A)
+                optX = A;
+            else if(arr[1] > maxX)
+                optX = maxX;
+            else
+                optX = arr[0];
+            
+            long cost = 0;
+            for(int i = 0; i < N; i++)
+                cost += Math.abs(arr[i] - (optX + (L * i)));
+            
+            println(cost);
+        }
+        
+    }
+    
+    
+    /************************ SOLUTION ENDS HERE ************************/
+    
+    
+    
+    
+    
+    /************************ TEMPLATE STARTS HERE **********************/
+    
+    public static void main(String[] args) throws IOException {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)), false);
+        st     = null;
+        solve();
+        reader.close();
+        writer.close();
+    }
+    
+    static BufferedReader reader;
+    static PrintWriter    writer;
+    static StringTokenizer st;
+    
+    static String next()
+    {while(st == null || !st.hasMoreTokens()){try{String line = reader.readLine();if(line == null){return null;}            
+    st = new StringTokenizer(line);}catch (Exception e){throw new RuntimeException();}}return st.nextToken();}
+    static String nextLine()  {String s=null;try{s=reader.readLine();}catch(IOException e){e.printStackTrace();}return s;}             
+    static int    nextInt()   {return Integer.parseInt(next());}
+    static long   nextLong()  {return Long.parseLong(next());}     
+    static double nextDouble(){return Double.parseDouble(next());}
+    static char   nextChar()  {return next().charAt(0);}
+    static int[]  nextIntArray(int n)         {int[] a= new int[n];   int i=0;while(i<n){a[i++]=nextInt();}  return a;}
+    static long[] nextLongArray(int n)        {long[]a= new long[n];  int i=0;while(i<n){a[i++]=nextLong();} return a;}    
+    static int[]  nextIntArrayOneBased(int n) {int[] a= new int[n+1]; int i=1;while(i<=n){a[i++]=nextInt();} return a;}            
+    static long[] nextLongArrayOneBased(int n){long[]a= new long[n+1];int i=1;while(i<=n){a[i++]=nextLong();}return a;}            
+    static void   print(Object o)  { writer.print(o);  }
+    static void   println(Object o){ writer.println(o);}
+    
+    /************************ TEMPLATE ENDS HERE ************************/
+    
+}
