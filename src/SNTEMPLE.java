f8e9a2ecb5b7c58fa7b1e6c0d09bb97dc47ef99e
bhishmaraj <matib275@gmail.com>
2017-05-28 15:58:01 +0530

    commit at 28/05/2017 15:58:01 IST

--- src/SNTEMPLE.java
+++ src/SNTEMPLE.java
@@ -11,10 +11,75 @@ class SNTEMPLE {
         
         int T = nextInt();
         while(T-->0) {
+            
             int N = nextInt();
             int arr[] = nextIntArray(N);
             int left[] = new int[N];
             int right[] = new int[N];
+            long total = 0;
+            for(int a : arr) total += a;
+            Arrays.fill(left, 1);
+            Arrays.fill(right, 1);
+            
+            {
+                int last = 0;
+                for(int i = 1; i <= N && arr[i - 1] >= i; i++) {
+                    last++;
+                }
+                left[last - 1] = last;
+                for(int i = 1; i < N; i++) {
+                    int ptr = i + last - 1;
+                    last--;
+                    while(ptr < N && arr[ptr] >= last + 1) {
+                        ptr++;
+                        last++;
+                    }
+                    left[i + last - 1] = Math.max(left[i + last - 1] , last);
+                }
+            }
+            
+            
+            {
+                int last = 0;
+                for(int i = 1; i <= N && arr[N - i] >= i; i++) {
+                    last++;
+                }
+                right[N - last] = last;
+                for(int i = N - 2; i >= 0; i--) {
+                    int ptr = i - last + 1;
+                    last--;
+                    while(ptr >= 0 && arr[ptr] >= last + 1) {
+                        ptr--;
+                        last++;
+                    }
+                    right[i - last + 1] = Math.max(right[i - last + 1] , last);
+                }
+            }
+            
+            int prev = left[N - 1];
+            for(int i = N - 2; i >= 0; i--) {
+                prev--;
+                left[i] = Math.max(left[i] , prev);
+                prev = Math.max(prev , left[i]);
+            }
+            
+            prev = right[0];
+            for(int i = 1; i < N; i++) {
+                prev--;
+                right[i] = Math.max(right[i] , prev);
+                prev = Math.max(prev , right[i]);
+            }
+            
+            /*
+            System.out.println("left  "  + Arrays.toString(left));
+            System.out.println("right " + Arrays.toString(right));
+            */
+            
+            int maxTop = 0;
+            for(int i = 0; i < N; i++)
+                maxTop = Math.max(maxTop , Math.min(left[i] , right[i]));
+            
+            println(total - 1L * maxTop * maxTop);
             
         }
         
---
+++ src/Stresstester_SNTEMPLE.java
@@ -0,0 +1,86 @@
+import java.util.*;
+import java.io.*;
+public class Stresstester_SNTEMPLE
+{
+
+
+    /************************ SOLUTION STARTS HERE ***********************/
+
+    private static void generator(OutputStream file , OutputStream judge , OutputStream cand){   //Writes a single instance for a test case
+
+        /*
+         * This is the random test generator which generates a single instance 
+         * use input object for writing into input.txt
+         * 
+         * 
+         */
+        Random rand = new Random();
+        StringBuilder dump = new StringBuilder();
+        dump.append(1 + "\n");
+        dump.append(N + "\n");
+        for(int i = 1; i <= N; i++)
+            dump.append((1 + rand.nextInt(H_MAX)) + " ");
+        
+        byte temp[] = dump.toString().getBytes();
+        try {
+            file.write(temp);
+            judge.write(temp);
+            cand.write(temp);
+            file.close();
+            judge.close();
+            cand.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int JUDGE(InputStream judge, InputStream cand) {
+        BufferedReader correct = new BufferedReader(new InputStreamReader(judge));
+        BufferedReader unchecked = new BufferedReader(new InputStreamReader(cand));
+        String read = null;
+        int line = 0;
+        try {
+            while ((read = correct.readLine()) != null) {
+                line++;
+                if (!read.equals(unchecked.readLine()))
+                    return line;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+
+
+    static int runs = 3;   //Number of times to run the stresstest bed
+    static int N = 10000;
+    static int H_MAX = 1000 * 1000 * 1000;
+    /************************ TEMPLATE STARTS HERE *********************/
+
+    public static void main(String []args) throws IOException {
+        while(runs-->0) {      
+            Process judgeProcess = Runtime.getRuntime().exec("java -cp /home/bhishmaraj/workspace/Online_Judge_V4/bin SNTEMPLE_SLOW");
+            Process candProcess  = Runtime.getRuntime().exec("java -cp /home/bhishmaraj/workspace/Online_Judge_V4/bin SNTEMPLE");
+            generator(new FileOutputStream("input.txt"), judgeProcess.getOutputStream(), candProcess.getOutputStream());
+            try {
+                judgeProcess.waitFor();
+                candProcess.waitFor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            int ret = JUDGE(judgeProcess.getInputStream(), candProcess.getInputStream());
+            if(ret > 0) {
+                System.err.println("WA at line " + ret);
+                System.exit(1);
+            }
+            else
+                System.out.println("PASS");
+        }
+        
+        System.out.println("ALL TESTS PASSED !!");
+    }    
+
+
+    /************************ TEMPLATE ENDS HERE ************************/
+}
