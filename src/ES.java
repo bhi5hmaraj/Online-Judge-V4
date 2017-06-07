a46a5f95e109f2c783ac49dae6b9ed76eb89c955
bhishmaraj <matib275@gmail.com>
2017-06-07 23:58:02 +0530

    commit at 07/06/2017 23:58:02 IST

--- src/ES.java
+++ src/ES.java
@@ -1,34 +1,48 @@
+import java.io.*;
 import java.math.*;
 import java.util.*;
 /*
 * Adapted from https://math.stackexchange.com/a/2053713
 */
 class ES {
-    static final BigDecimal TWO_ = new BigDecimal("2");
-    static final MathContext c = new MathContext(150);
-    static BigDecimal sum(BigDecimal n) {
-        return ((n.multiply(n , c)).add(n , c)).divide(TWO_ , c);
+    static final BigInteger TWO = BigInteger.valueOf(2);
+    static final BigDecimal TWO_ = new BigDecimal("2.0");
+    static int rec = 0;
+    static BigInteger sum(BigInteger n) {
+        return ((n.multiply(n)).add(n)).divide(TWO);
     }
-    static BigDecimal S(BigDecimal alpha , BigDecimal n) {
-        System.out.println(alpha + " " + n);
-        if(n.equals(BigDecimal.ZERO))
-            return BigDecimal.ZERO;
+    static BigInteger S(BigDecimal alpha , BigInteger n) {
+         System.out.println("alpha " + alpha + " n " + n);
+        rec++;
+        if(n.equals(BigInteger.ZERO))
+            return BigInteger.ZERO;
         if(alpha.compareTo(TWO_) >= 0) {
-            BigDecimal frac = alpha.remainder(BigDecimal.ONE , c);
-            BigDecimal mult = alpha.subtract(frac.add(BigDecimal.ONE , c) , c);
-            System.out.println("mult " + mult);
-            return S(alpha.subtract(mult , c), n).add((sum(n).multiply(mult , c)) , c);
+            BigDecimal frac = alpha.remainder(BigDecimal.ONE);
+            int mult = alpha.subtract(BigDecimal.ONE).intValue();
+            return S(BigDecimal.ONE.add(frac), n).add((sum(n).multiply(BigInteger.valueOf(mult))));
         }
         else {
-            BigDecimal cache = alpha.subtract(BigDecimal.ONE , c);
-            BigDecimal beta = alpha.divide(cache , c);
-            BigDecimal n_ = n.multiply(cache , c).round();
-            BigDecimal m = n.add(n_ , c);
-            return sum(m).subtract(S(beta, n_) , c);
+            BigDecimal cache = alpha.subtract(BigDecimal.ONE);
+//            System.out.println(cache);
+            BigDecimal beta = alpha.divide(cache ,200 , BigDecimal.ROUND_FLOOR);
+            BigInteger n_ = new BigDecimal(n).multiply(cache).toBigInteger();
+            BigInteger m = n.add(n_);
+            return sum(m).subtract(S(beta, n_));
         }
     }
-    public static void main(String[] args) {
-        BigDecimal E = new BigDecimal("2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435729003342952605956");
-        System.out.println(S(E, BigDecimal.TEN.pow(1)).toBigInteger());
+    public static void main(String[] args) throws IOException {
+        // System.out.println(S(Math.E, new BigInteger(new Scanner(System.in).nextLine())));
+        try {
+            // System.out.println();
+            BigDecimal E = new BigDecimal("2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435729003342952605956");
+            BigInteger ans = S(E, BigInteger.valueOf(10).pow(100));
+            System.out.println(ans);
+            System.out.println("Depth " + rec); 
+        }
+        catch(Error SOE) {
+            System.out.println("Stack Overflow !!! \n" + 
+                                "Exiting...");
+        }
     }
 }
+//
