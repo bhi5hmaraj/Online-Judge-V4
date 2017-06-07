d7e79b2a542878d07700401d7f3b06a46653addf
bhishmaraj <matib275@gmail.com>
2017-06-07 13:58:01 +0530

    commit at 07/06/2017 13:58:01 IST

--- src/ES.java
+++ src/ES.java
@@ -7,31 +7,25 @@ import java.util.*;
 class ES {
     static final BigInteger TWO = BigInteger.valueOf(2);
     static BigInteger sum(BigInteger n) {
-        return n.multiply(n).add(n).divide(TWO);
+        return ((n.multiply(n)).add(n)).divide(TWO);
     }
     static BigInteger S(double alpha , BigInteger n) {
+        System.out.println("alpha " + alpha + " n " + n);
         if(n.equals(BigInteger.ZERO))
             return BigInteger.ZERO;
-        if(alpha >= 2)
-            return S(alpha - 1, n).add(sum(n));
+        if(alpha >= 2) {
+            double frac = alpha - Math.floor(alpha);
+            int mult = (int) Math.round(alpha - (1.0 + frac));
+            return S(alpha - mult, n).add((sum(n).multiply(BigInteger.valueOf(mult))));
+        }
         else {
             double beta = alpha / (alpha - 1);
-            BigInteger n_ = new BigDecimal(n).multiply(BigDecimal.valueOf(alpha - 1)).setScale(0, BigDecimal.ROUND_FLOOR).toBigInteger();
+            BigInteger n_ = new BigDecimal(n).multiply(BigDecimal.valueOf(alpha - 1)).toBigInteger();
             BigInteger m = n.add(n_);
             return sum(m).subtract(S(beta, n_));
         }
     }
     public static void main(String[] args) throws IOException {
-        new Thread(null, new Runnable() {
-            public void run() {
-                new ES().run();
-            }
-        }, "Increase Stack", 1 << 25).start();
-
-    }
-    void run(){ 
-        long start = System.nanoTime();
-        System.out.println(S(Math.E, BigInteger.TEN.pow(200)));
-        System.out.println("Time " + (System.nanoTime() - start)/1e9);
+        System.out.println(S(2.7182818284590452353602874713527, new BigInteger(new Scanner(System.in).nextLine())));
     }
 }
