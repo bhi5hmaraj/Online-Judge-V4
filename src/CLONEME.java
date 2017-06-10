075ec60da2975937d7aa05ac88fa1a2c3a99ad20
bhishmaraj <matib275@gmail.com>
2017-06-10 21:58:01 +0530

    commit at 10/06/2017 21:58:01 IST

--- src/CLONEME.java
+++ src/CLONEME.java
@@ -21,17 +21,78 @@ class CLONEME {
     }
     
     static class SegTreeNode {
-        int freq , hash1 , hash2;
+        int size , hash1 , hash2;
         SegTreeNode left , right;
+        public SegTreeNode(int size , int hash1 , int hash2) {
+            this.size = size;
+            this.hash1 = hash1;
+            this.hash2 = hash2;
+        }
+        
+        @Override
+        public String toString() {
+            return String.format("[sz = %d , h1 = %d , h2 = %d", size , hash1 , hash2);
+        }
+    }
+    
+    static SegTreeNode persistent[];
+    static SegTreeNode initSegTree(int L , int R) {
+        if(L == R)
+            return new SegTreeNode(0, 0, 0);
+        else {
+            SegTreeNode newNode = new SegTreeNode(0, 0, 0);
+            int mid = (L + R) >> 1;
+            newNode.left = initSegTree(L, mid);
+            newNode.right = initSegTree(mid + 1, R);
+            return newNode;
+        }
+    }
+    static SegTreeNode initSegTree(SegTreeNode prev , int L , int R , int val) {
+        if(L == R) 
+            return new SegTreeNode(1, pow1[val], pow2[val]);
+        else {
+            int mid = (L + R) >> 1;
+            SegTreeNode l = prev.left;
+            SegTreeNode r = prev.right;
+            if(val <= mid)
+                l = initSegTree(prev , L , mid , val);
+            else
+                r = initSegTree(prev, mid + 1, R, val);
+            SegTreeNode newNode = new SegTreeNode(l.size + r.size, (l.hash1 + r.hash1) % m1, (l.hash2 + r.hash2) % m2);
+            newNode.left = l;
+            newNode.right = r;
+            return newNode;
+        }
+    }
+    
+    static boolean equals(SegTreeNode... n) {
+        
     }
     
     private static void solve() {
         
         int T = nextInt();
         while(T-->0) {
+            int N = nextInt();
+            int Q = nextInt();
+            int arr[] = nextIntArrayOneBased(N);
+            persistent[0] = initSegTree(1, N);
+            for(int i = 1; i <= N; i++)
+                persistent[i] = initSegTree(persistent[i - 1], 1, N, arr[i]);
             
-            
-            
+            while(Q-->0) {
+                SegTreeNode L1 = persistent[nextInt()];
+                SegTreeNode R1 = persistent[nextInt()];
+                SegTreeNode L2 = persistent[nextInt()];
+                SegTreeNode R2 = persistent[nextInt()];
+                
+                if((R1.hash1 - L1.hash1 + m1) % m1 == (R2.hash1 - L2.hash1 + m1) % m1 &&
+                   (R1.hash2 - L1.hash2 + m2) % m2 == (R2.hash2 - L2.hash2 + m2) % m2)
+                    println("YES");
+                else {
+                    
+                }
+            }
         }
         
     }
