501acd50828c5b74c3f31d906064286236c8c8ca
bhishmaraj <matib275@gmail.com>
2017-06-20 20:58:02 +0530

    commit at 20/06/2017 20:58:02 IST

--- src/Timofeyandatree.java
+++ src/Timofeyandatree.java
@@ -7,14 +7,105 @@ public class Timofeyandatree {
     /************************ SOLUTION STARTS HERE ************************/
     
     
-    ArrayList<Integer>[] adj;
+    static ArrayList<Integer>[] adj;
+
+    static class DSU {
+        private int parent[];
+        private int size[];
+        private int cnt;
+
+        DSU(int length) {
+            this.cnt = length;
+            parent = new int[length + 10];
+            size = new int[length + 10];
+            Arrays.fill(size, 1);
+            for (int i = 0; i < parent.length; i++)
+                parent[i] = i;
+        }
+
+        int root(int p) {
+            while(p != parent[p]) p = parent[p];
+            return p;
+        }
+
+        int sizeOf(int p) {
+            return size[root(p)];
+        }
+
+        boolean connected(int u, int v) {
+            return root(u) == root(v);
+        }
+
+        int components() {
+            return cnt;
+        }
+
+        void union(int u, int v) {
+            if (!connected(u, v)) {
+                cnt--;
+                int rootU = root(u);
+                int rootV = root(v);
+                if (size[rootU] < size[rootV]) {
+                    parent[rootU] = rootV;
+                    size[rootV] += size[rootU];
+                } else {
+                    parent[rootV] = rootU;
+                    size[rootU] += size[rootV];
+                }
+            }
+        }
+    }
+
+    static DSU unionFind;
+    static int color[];
+    static boolean marked[];
+    static void dfs(int u) {
+        marked[u] = true;
+        for(int v : adj[u])
+            if(!marked[v] && color[u] == color[v]) {
+                unionFind.union(u, v);
+                dfs(v);
+            }
+    }
     
+    @SuppressWarnings("unchecked")
     private static void solve() {
         
         
+        int V = nextInt();
+        int E = V - 1;
+        adj = new ArrayList[V + 1];
+        for(int i = 1; i <= V; i++)
+            adj[i] = new ArrayList<>();
         
+        while(E-->0) {
+            int u = nextInt();
+            int v = nextInt();
+            adj[u].add(v);
+            adj[v].add(u);
+        }
         
+        color = nextIntArrayOneBased(V);
+        marked = new boolean[V + 1];
+        unionFind = new DSU(V);
+        for(int i = 1; i <= V; i++)
+            if(!marked[i])
+                dfs(i);
         
+        for(int i = 1; i <= V; i++) {
+            int sz = unionFind.sizeOf(i);
+            for(int j : adj[i])
+                if(color[i] != color[j])
+                    sz += unionFind.sizeOf(j);
+            
+            if(sz == V) {
+                println("YES");
+                println(i);
+                return;
+            }
+        }
+        
+        println("NO");
         
     }
     
