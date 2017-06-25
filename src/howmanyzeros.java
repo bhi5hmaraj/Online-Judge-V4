447d02a9a85b16a9aed6ce7dade3eb7adf11ff98
bhishmaraj <matib275@gmail.com>
2017-06-25 18:58:01 +0530

    commit at 25/06/2017 18:58:01 IST

--- src/howmanyzeros.java
+++ src/howmanyzeros.java
@@ -31,19 +31,22 @@ public class howmanyzeros {
         for(int i = 1 , len = str.length; i < len - 1; i++)
             cnt += 9L * numOfZeros[len - i - 1];
         
-        cnt += 1L * (str[0] - '0' - 1) * numOfZeros[str.length - 1];
-        System.out.println("cnt0 " + cnt);
-        for(int i = 1 , len = str.length; i < len - 1; i++) {
+        int i = 0;
+        cnt += 1L * (str[i++] - '0' - 1) * numOfZeros[str.length - 1];
+        // System.out.println("cnt0 " + cnt);
+        for(int len = str.length; i < len - 1; i++) {
             int dig = str[i] - '0';
             if(dig > 0) {
-                cnt += 1L * (dig) * numOfZeros[len - i - 1];
-                cnt += 1L * (zerosCnt + dig)  * pow10[len - i - 1];
+                cnt += 1L * (dig) * numOfZeros[len - i - 1];    // fix 0 to d-1 in i
+                cnt += 1L * (zerosCnt * (dig - 1))  * pow10[len - i - 1];   // for 1 to d - 1
+                cnt += 1L * (zerosCnt + 1) * pow10[len - i - 1];    // for 0
             }
             else
                 zerosCnt++;
+            // System.out.println("i " + i + " zerocnt " + zerosCnt + " cnt " + cnt);
         }
-        System.out.println("before last " + cnt);
-        return cnt + (zerosCnt * (str[str.length - 1] - '0' + 1)) + 1;
+        // System.out.println("before last " + cnt);
+        return i < str.length ? cnt + (zerosCnt * (str[str.length - 1] - '0' + 1)) + 1 : cnt;
     }
     
     private static void solve() {
@@ -51,19 +54,13 @@ public class howmanyzeros {
         pow10[0] = 1;
         for(int i = 1; i <= MAX_DIGIT; i++)
             pow10[i] = pow10[i - 1] * 10;
-        /*
-        numOfZeros = new long[MAX_DIGIT + 1];
-        numOfZeros[1] = 1;
-        long pow9 = 9;
-        for(int i = 2; i <= MAX_DIGIT; i++ , pow9 *= 9)
-            numOfZeros[i] = 10 * numOfZeros[i - 1] + pow9;
-        */
-        /*
+        
         long L , R;
-        while((L = nextLong()) != -1 && (R = nextLong()) != -1) {
-            
-        }
-        */
+        while((L = nextLong()) != -1 && (R = nextLong()) != -1) 
+            println(countZeros(R) - countZeros(L - 1));
+        
+        
+        /*
         int x = nextInt();
         System.out.println(countZeros(x));
         
@@ -73,68 +70,9 @@ public class howmanyzeros {
                             .forEach(ch -> cnt[0] += ch == '0' ? 1 : 0));
         
         System.out.println("brute " + cnt[0]);
-        
+        */
     }
     
-    static char num[];
-    static long memo[][][];
-    
-    static long count(int idx , int any , int numStarted) {
-        System.out.println("idx " + idx + " " + (idx < num.length ? (char) num[idx] : "") + " any " + any + " start " + numStarted);
-        
-        long ret = 0;
-        if(idx == num.length)
-            return 0;
-        else if(memo[idx][any][numStarted] != -1)
-            return memo[idx][any][numStarted];
-        else if(any > 0) {
-            if(numStarted > 0)
-                ret = numOfZeros[num.length - idx];
-            else {
-                ret = count(idx + 1, any, numStarted);
-                ret += 9L * count(idx + 1, 1, 1);
-            }
-        }
-        else {
-            long cnt = 0;
-            int d = num[idx] - '0';
-            if(d == 0)
-                cnt = numStarted + count(idx + 1, any, numStarted);
-            else {
-                cnt = numStarted + count(idx + 1, 1, numStarted); // for 0 
-                cnt += 1L * (d - 1) * count(idx + 1, 1, 1);       // < d
-                cnt += count(idx + 1, 0, 1);                      // = d
-            }
-            
-            ret = cnt;
-        }
-        System.out.println("idx " + idx + " any " + any + " start " + numStarted + " ret " + ret);
-        return memo[idx][any][numStarted] = ret;
-      
-    }
-    
-    static long count(long n) {
-        if(n < 0) return 0;
-        memo = new long[MAX_DIGIT][2][2];
-        for(long a[][] : memo) for(long b[] : a) Arrays.fill(b, -1);
-        num = String.valueOf(n).toCharArray();
-        return 1 + count(0, 0, 0);
-    }
-    
-    private static void solve2() {
-        
-        int x = nextInt();
-        System.out.println("count " + count(x));
-        
-        int[] cnt = new int[1];
-        IntStream.rangeClosed(0, x).
-            forEach(i -> String.valueOf(i).chars()
-                            .forEach(ch -> cnt[0] += ch == '0' ? 1 : 0));
-        
-        System.out.println("brute " + cnt[0]);
-        
-        
-    }
     
     /************************ SOLUTION ENDS HERE ************************/
     
