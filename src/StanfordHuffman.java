9f55f59c25496010235f88aa939955f9fb4c8b7a
bhishmaraj <matib275@gmail.com>
2017-07-11 15:58:01 +0530

    commit at 11/07/2017 15:58:01 IST

---
+++ src/StanfordHuffman.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.*;
+public class StanfordHuffman {
+    
+    
+    
+    /************************ SOLUTION STARTS HERE ************************/
+    
+    
+    private static void solve() {
+        
+        
+        int n = nextInt();
+        PriorityQueue<long[]> pq = new PriorityQueue<>((a1 , a2) -> Long.compare(a1[0], a2[0]));
+        
+        while(n-->0)
+            pq.add(new long[]{nextLong() , 0});
+        
+        while(pq.size() > 1) {
+            long a1[] = pq.remove();
+            long a2[] = pq.remove();
+            pq.add(new long[]{a1[0] + a2[0] , 1 + Math.min(a1[1] , a2[1])});
+        }
+        
+        println(pq.remove()[1]);
+        
+        
+    }
+    
+    
+    
+    /************************ SOLUTION ENDS HERE ************************/
+    
+    
+    
+    
+    
+    /************************ TEMPLATE STARTS HERE **********************/
+    
+    public static void main(String[] args) throws IOException {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)), false);
+        st     = null;
+        solve();
+        reader.close();
+        writer.close();
+    }
+    
+    static BufferedReader reader;
+    static PrintWriter    writer;
+    static StringTokenizer st;
+    
+    static String next()
+    {while(st == null || !st.hasMoreTokens()){try{String line = reader.readLine();if(line == null){return null;}            
+    st = new StringTokenizer(line);}catch (Exception e){throw new RuntimeException();}}return st.nextToken();}
+    static String nextLine()  {String s=null;try{s=reader.readLine();}catch(IOException e){e.printStackTrace();}return s;}             
+    static int    nextInt()   {return Integer.parseInt(next());}
+    static long   nextLong()  {return Long.parseLong(next());}     
+    static double nextDouble(){return Double.parseDouble(next());}
+    static char   nextChar()  {return next().charAt(0);}
+    static int[]  nextIntArray(int n)         {int[] a= new int[n];   int i=0;while(i<n){a[i++]=nextInt();}  return a;}
+    static long[] nextLongArray(int n)        {long[]a= new long[n];  int i=0;while(i<n){a[i++]=nextLong();} return a;}    
+    static int[]  nextIntArrayOneBased(int n) {int[] a= new int[n+1]; int i=1;while(i<=n){a[i++]=nextInt();} return a;}            
+    static long[] nextLongArrayOneBased(int n){long[]a= new long[n+1];int i=1;while(i<=n){a[i++]=nextLong();}return a;}            
+    static void   print(Object o)  { writer.print(o);  }
+    static void   println(Object o){ writer.println(o);}
+    
+    /************************ TEMPLATE ENDS HERE ************************/
+    
+}
