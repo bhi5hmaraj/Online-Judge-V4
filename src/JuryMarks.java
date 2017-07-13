7cc9d4f94d79fcadc411d448e11b62e4cd6ea7cd
bhishmaraj <matib275@gmail.com>
2017-07-13 21:58:01 +0530

    commit at 13/07/2017 21:58:01 IST

---
+++ src/JuryMarks.java
@@ -0,0 +1,83 @@
+import java.util.*;
+import java.io.*;
+public class JuryMarks {
+    
+    
+    
+    /************************ SOLUTION STARTS HERE ************************/
+    
+    
+    private static void solve() {
+        
+        
+        int k = nextInt();
+        int n = nextInt();
+        
+        int a[] = nextIntArray(k);
+        int b[] = nextIntArray(n);
+        
+        int prefixSum[] = new int[k];
+        prefixSum[0] = a[0];
+        
+        for(int i = 1; i < k; i++)
+            prefixSum[i] += prefixSum[i - 1] + a[i];
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i = 0; i < k; i++)
+            set.add(b[0] - prefixSum[i]);
+        
+        for(int j = 1; j < n; j++) {
+            HashSet<Integer> temp = new HashSet<>();
+            for(int i = 0; i < k; i++) 
+                if(set.contains(b[j] - prefixSum[i]))
+                    temp.add(b[j] - prefixSum[i]);
+            
+            set = temp;
+        }
+        
+        println(set.size());
+        
+    }
+    
+    
+    
+    /************************ SOLUTION ENDS HERE ************************/
+    
+    
+    
+    
+    
+    /************************ TEMPLATE STARTS HERE **********************/
+    
+    public static void main(String[] args) throws IOException {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)), false);
+        st     = null;
+        solve();
+        reader.close();
+        writer.close();
+    }
+    
+    static BufferedReader reader;
+    static PrintWriter    writer;
+    static StringTokenizer st;
+    
+    static String next()
+    {while(st == null || !st.hasMoreTokens()){try{String line = reader.readLine();if(line == null){return null;}            
+    st = new StringTokenizer(line);}catch (Exception e){throw new RuntimeException();}}return st.nextToken();}
+    static String nextLine()  {String s=null;try{s=reader.readLine();}catch(IOException e){e.printStackTrace();}return s;}             
+    static int    nextInt()   {return Integer.parseInt(next());}
+    static long   nextLong()  {return Long.parseLong(next());}     
+    static double nextDouble(){return Double.parseDouble(next());}
+    static char   nextChar()  {return next().charAt(0);}
+    static int[]  nextIntArray(int n)         {int[] a= new int[n];   int i=0;while(i<n){a[i++]=nextInt();}  return a;}
+    static long[] nextLongArray(int n)        {long[]a= new long[n];  int i=0;while(i<n){a[i++]=nextLong();} return a;}    
+    static int[]  nextIntArrayOneBased(int n) {int[] a= new int[n+1]; int i=1;while(i<=n){a[i++]=nextInt();} return a;}            
+    static long[] nextLongArrayOneBased(int n){long[]a= new long[n+1];int i=1;while(i<=n){a[i++]=nextLong();}return a;}            
+    static void   print(Object o)  { writer.print(o);  }
+    static void   println(Object o){ writer.println(o);}
+    
+    /************************ TEMPLATE ENDS HERE ************************/
+    
+}
