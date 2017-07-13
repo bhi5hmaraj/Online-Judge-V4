ef8ddea099186a888004aa089f9d555fc3820e89
bhishmaraj <matib275@gmail.com>
2017-07-13 14:58:01 +0530

    commit at 13/07/2017 14:58:01 IST

--- src/poj_2292.java
+++ src/poj_2292.java
@@ -9,19 +9,66 @@ public class poj_2292 {
     static final char chars[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 
                                  'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '+', '*', '/', '?'};
     
+    static final int INF = (int) 1e9;
     static final int CUT = 11;
+    static int freq[];
+    static int memo[][];
     
     static int findOpt(int idx , int off) {
-        if(idx >= CUT) {
+        if(idx > CUT) 
+            return off == chars.length ? 0 : INF;
+        else if(off >= chars.length)
+            return INF;
+        else if(memo[idx][off] != -1)
+            return memo[idx][off];
+        else {
+            int minCost = INF;
+            int curr = 0;
+            for(int i = 1; i <= chars.length - off; i++) {
+                curr += i * freq[off + i - 1];
+                minCost = Math.min(minCost , curr + findOpt(idx + 1, off + i));
+            }
             
+            return memo[idx][off] = minCost;
+        }
+    }
+    
+    static int pos(char ch) {
+        if(ch >= 'a' && ch <= 'z')
+            return ch - 'a';
+        else {
+            switch(ch) {
+            default:
+                return -1;
+            case '+':
+                return 26;
+            case '*':
+                return 27;
+            case '/':
+                return 28;
+            case '?':
+                return 29;
+            }
         }
     }
     
     private static void solve() {
         
-        
-        
-        
+        int T = nextInt();
+        while(T-->0) {
+            int M = nextInt();
+            freq = new int[chars.length];
+            while(M-->0)
+                for(char ch : nextLine().toCharArray())
+                    freq[pos(ch)]++;
+            
+            memo = new int[CUT + 1][chars.length];
+            for(int a[] : memo) Arrays.fill(a, -1);
+            
+            findOpt(0, 0);
+            StringBuilder ans = new StringBuilder();
+            
+        }
         
         
     }
