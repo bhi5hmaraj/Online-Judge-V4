7074c0a0846f0434e4bb31a032bd3d214c8f8bef
bhishmaraj <matib275@gmail.com>
2017-07-30 18:57:05 +0530

    commit at 30/07/2017 18:57:05 IST

--- src/MaximumValue.java
+++ src/MaximumValue.java
@@ -38,6 +38,59 @@ public class MaximumValue {
 
     }
 
+    static class SegmentTree  { // Implemented to store min in a range , point update and range query
+        int tree[];
+        int len;
+        int size;
+        SegmentTree(int arr[]) { // arr should be a 1 based array
+            len = arr.length - 1;
+            size = 1 << (32 - Integer.numberOfLeadingZeros(len - 1) + 1);  // ceil(log(len)) + 1
+            tree = new int[size];
+            build(arr, 1, 1, len);
+        }
+        void update(int node,int idx,int val,int nl,int nr) {
+            if(nl == nr && nl == idx)
+                tree[node] = val;
+            else {
+                int mid = (nl + nr) >> 1;
+                if(idx <= mid)
+                    update(2*node, idx , val ,nl , mid);
+                else
+                    update((2*node) + 1, idx ,val , mid + 1, nr);
+
+                tree[node] = Math.max(tree[2*node],tree[(2 * node) + 1]);
+            }
+        }
+        void update(int idx , int val){
+            update(1, idx, val, 1, len);
+        }
+        int getMax(int L , int R){
+            return query(1, L, R, 1, len);
+        }
+        int query(int node , int L , int R, int nl, int nr) {
+            int mid = (nl + nr) >> 1;
+            if(nl == L && nr == R)
+                return tree[node];
+            else if(R <= mid)
+                return query(2 * node, L, R, nl, mid);
+            else if(L > mid)
+                return query((2*node)+1, L, R, mid + 1 , nr);
+            else
+                return Math.max(query(2*node, L, mid , nl , mid) ,  query((2*node)+1, mid+1, R , mid+1,nr));
+        }
+        void build(int arr[],int node,int L,int R) {
+            if(L == R)
+                tree[node] = arr[L];
+            else
+            {
+                int mid = (L + R) >> 1;
+                build(arr, 2 * node, L, mid);
+                build(arr, (2 * node) + 1, mid + 1, R);
+                tree[node] = Math.max(tree[2*node] , tree[(2 * node) + 1]);
+            }
+        }
+    }
+
     private static void solve() {
         
         final int MAX = (int) 1e6;
@@ -50,14 +103,17 @@ public class MaximumValue {
             arr[a] = a;
         }
         
-        SparseTable1D sparseTable = new SparseTable1D(arr);
+        SparseTable1D sparseTable = new SparseTable1D(arr); // 312 ms
+        // SegmentTree segTree = new SegmentTree(arr); // 404 ms
         int max = 0;
         for(int i = 1; i <= MAX; i++)
             if(arr[i] > 0) {
                 int localMax = 0;
                 for(int j = 2 * i; j <= MAX && localMax != i - 1; j += i) 
                     localMax = Math.max(localMax , sparseTable.getMax(j - i, j - 1) % i);
-                
+                if(MAX % i != 0)
+                    localMax = Math.max(localMax , sparseTable.getMax(MAX - (MAX % i) + 1, MAX) % i);
+  
                 max = Math.max(max , localMax);
             }
         
@@ -76,9 +132,12 @@ public class MaximumValue {
     
     public static void main(String[] args) throws IOException {
         reader = new BufferedReader(new InputStreamReader(System.in));
+        // reader = new BufferedReader(new FileReader("/home/bhishmaraj/Documents/CODE/max_value.txt"));
         writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)), false);
         st     = null;
+        // long st = System.nanoTime();
         solve();
+        // println("Time : " + (System.nanoTime() - st) / 1e9);
         reader.close();
         writer.close();
     }
