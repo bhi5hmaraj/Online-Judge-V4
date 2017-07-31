41d08be22d6383656f725d324cea8ed3643df0c6
bhishmaraj <matib275@gmail.com>
2017-07-31 23:58:01 +0530

    commit at 31/07/2017 23:58:01 IST

--- src/Palindromiccharacteristics.java
+++ src/Palindromiccharacteristics.java
@@ -5,46 +5,29 @@ public class Palindromiccharacteristics  {
     
     
     /************************ SOLUTION STARTS HERE ************************/
-    static final int p1 = 100151 , m1 = (int) 1e9 + 7;  // Twin Primes
-    static final int p2 = p1 + 2 , m2 = m1 + 2;
-    static final int MAX = (int) 6e3 + 10;
-    static final int pow1[] = new int[MAX + 1];
-    static final int pow2[] = new int[MAX + 1];
-    static int modPow(long a , long b , long mod) {
-        long pow = 1;
-        while(b > 0) {
-            if((b & 1L) == 1)
-                pow = ((pow * a) % mod);
-
-            a = ((a * a) % (mod));
-            b >>= 1;
-        }
-        return (int) pow;
-    }
-    static int modInverse(long n , long mod)  {return modPow(n, mod - 2 , mod);} // Fermat's little theorem
+    static final long p = 100151;
+    static final int MAX = (int) 5e3 + 10;
+    static final long pow[] = new long[MAX + 1];
+    // implicit mod (2^63)
     static {
-        pow1[0] = pow2[0] = 1;
-        for(int i = 1; i <= MAX; i++) {
-            pow1[i] = (int) ((1L * pow1[i - 1] * p1) % (long)(m1));
-            pow2[i] = (int) ((1L * pow2[i - 1] * p2) % (long)(m2));
-        }
+        pow[0] = 1;
+        for(int i = 1; i <= MAX; i++) 
+            pow[i] = pow[i - 1] * p;
     }
     
-    static int hash1[] , hash2[];
-    static int cache1[] , cache2[];
-    static int hold[][];
-    static void subHash(int l , int r , int idx) {
+    static long hash[];
+    
+    static long subHash(int l , int r) {
         l++;
         r++;
-        hold[idx][0] = (int) ((1L * cache1[l - 1] * ((hash1[r] - hash1[l - 1] + m1) % m1)) % (long)(m1));
-        hold[idx][1] = (int) ((1L * cache2[l - 1] * ((hash2[r] - hash2[l - 1] + m2) % m2)) % (long)(m2));
+        return hash[r] - hash[l - 1] * pow[r - l + 1];
     }
     
-    
+
     private static void solve() {
         
         char str[] = nextLine().toCharArray();
-        long st = System.nanoTime();
+//         long st = System.nanoTime();
         int n = str.length;
         int log = 32 - Integer.numberOfLeadingZeros(n);
         
@@ -58,22 +41,11 @@ public class Palindromiccharacteristics  {
         for(int len = 3; len <= n; len++)
             for(int i = 0; i + len - 1 < n; i++)
                 isPalin[i][i + len - 1] = isPalin[i + 1][i + len -2] && str[i] == str[i + len - 1];
-        hold = new int[2][2];
-        hash1 = new int[n + 1];
-        hash2 = new int[n + 1];
-        cache1 = new int[n + 1];
-        cache2 = new int[n + 1];
-        hash1[1] = hash2[1] = str[0];
-
-        for(int i = 2; i <= n; i++) {
-            hash1[i] = (hash1[i - 1] + ((int) ((1L * pow1[i - 1] * str[i - 1]) % (long)(m1)))) % m1;
-            hash2[i] = (hash2[i - 1] + ((int) ((1L * pow2[i - 1] * str[i - 1]) % (long)(m2)))) % m2;
-        }
         
-        for(int i = 0; i < n; i++) {
-            cache1[i] = modInverse(pow1[i], m1);
-            cache2[i] = modInverse(pow2[i], m2);
-        }
+        hash = new long[n + 1];
+        hash[1] = str[0];
+        for(int i = 2; i <= n; i++) 
+            hash[i] = hash[i - 1] * p + str[i - 1];
         
         int cnt[] = new int[n];
         for(int len = 1; len <= n; len++)
@@ -81,16 +53,16 @@ public class Palindromiccharacteristics  {
                 cnt[0] += isPalin[i][i + len - 1] ? 1 : 0;
                 DP[i][i + len - 1] = isPalin[i][i + len - 1];
             }
+        
         for(int k = 1; k < log; k++) {
             boolean t[][] = new boolean[n][n];
             for(int len = 2; len <= n; len++)
                 for(int i = 0; i + len - 1 < n; i++) {
                     int L = i;
                     int R = i + len - 1;
-                    subHash(L, L + (len / 2) - 1 , 0);
-                    subHash(R - (len / 2) + 1, R , 1);
-                    if(hold[0][0] == hold[1][0] && hold[0][1] == hold[1][1] && DP[L][L + (len / 2) - 1] && 
-                                                           DP[R - (len / 2) + 1][R]) {
+                    long lH = subHash(L, L + (len / 2) - 1 );
+                    long rH = subHash(R - (len / 2) + 1, R );
+                    if(lH == rH && DP[L][L + (len / 2) - 1] && DP[R - (len / 2) + 1][R]) {
                         t[L][R] = true;
                         cnt[k]++;
                     }
@@ -102,7 +74,8 @@ public class Palindromiccharacteristics  {
         for(int a : cnt)
             print(a + " ");
 
-        println("\nTime : " + (System.nanoTime() - st) / 1e9);
+//        println("\nTime : " + (System.nanoTime() - st) / 1e9);
+         
 
     }
     
