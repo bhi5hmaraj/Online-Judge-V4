f417c07130a30a9ac8a88c437d2b19e30ec9dffa
bhishmaraj <matib275@gmail.com>
2017-08-05 18:58:02 +0530

    commit at 05/08/2017 18:58:02 IST

--- src/poj_2488.java
+++ src/poj_2488.java
@@ -6,11 +6,62 @@ public class poj_2488 {
     
     /************************ SOLUTION STARTS HERE ************************/
     
-    
+    static int n , m;
+    static int dx[] = {-1 , 1 , -2 , 2 , -2 , 2 , -1 , 1};
+    static int dy[] = {-2 , -2 , -1 , -1 , 1 , 1 , 2 , 2};
+    static boolean marked[][];
+    static ArrayDeque<String> ans;
+    static String convert(int i , int j) {
+        return String.valueOf((char)('A' + j)) + (i + 1);
+    }
+    static boolean isValid(int i , int j) {
+        return i >= 0 && i < n && j >= 0 && j < m;
+    }
+    static boolean dfs(int x , int y , int remain) {
+        boolean flag = false;
+        if(remain == 1) 
+            flag = true;
+        else {
+            marked[x][y] = true;
+            for(int choice = 0 ; choice < 8; choice++) {
+                int nx = x + dx[choice];
+                int ny = y + dy[choice];
+                if(isValid(nx, ny) && !marked[nx][ny]) {
+                    if((flag = dfs(nx, ny, remain - 1)))
+                        break;
+                }
+            }
+            marked[x][y] = false;
+        }
+        if(flag)
+            ans.push(convert(x, y));
+        return flag;
+    }
     private static void solve() {
         
         
-        
+        int T = nextInt();
+        for(int tc = 1; tc <= T; tc++) {
+            
+            // row - n - number , col - m - alphabet
+            n = nextInt();
+            m = nextInt();
+            marked = new boolean[n][m];
+            ans = new ArrayDeque<String>();
+            outer:
+            for(int i = 0; i < n; i++)
+                for(int j = 0; j < m; j++)
+                    if(dfs(i, j, n * m))
+                        break outer;
+            
+            println("Scenario #" + tc + ":");
+            if(ans.isEmpty())
+                print("impossible");
+            else
+                for(String s : ans)
+                    print(s);
+            println('\n');
+        }
         
         
         
