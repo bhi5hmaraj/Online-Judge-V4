5f1d3144a12e8ca0db0f453182b5e2b2fc37f575
bhishmaraj <matib275@gmail.com>
2017-08-14 22:58:01 +0530

    commit at 14/08/2017 22:58:01 IST

--- src/MathisonAndThePeculiarSums.java
+++ src/MathisonAndThePeculiarSums.java
@@ -6,7 +6,7 @@ public class MathisonAndThePeculiarSums {
     
     /************************ SOLUTION STARTS HERE ************************/
     
-    static ArrayList<Integer>[] adj;
+    static int adj[][];
     static int wt[] , inv[];
     static final int mod = (int) 1e9 + 7;
     // Courtesy : UWI ( adjacency list using Jagged Arrays )
@@ -26,21 +26,25 @@ public class MathisonAndThePeculiarSums {
         return g;
     }
     static void compress() {
-        /*
-        TreeSet<Integer> set = new TreeSet<>();
-        for(int i = 1; i < wt.length; i++)
-            set.add(wt[i]);
-        HashMap<Integer , Integer> map = new HashMap<>();
-        set.stream().forEachOrdered(a -> map.put(a, map.size()));
-        inv = new int[map.size()];
-        for(int i = 1; i < wt.length; i++) {
-            inv[map.get(wt[i])] = wt[i];
-            wt[i] = map.get(wt[i]);
-        }
-        */
         int temp[] = Arrays.copyOf(wt, wt.length);
         Arrays.sort(temp);
-        throw new RuntimeException();
+        int sz = 0;
+        for(int i = 0; i < temp.length; ) {
+            int curr = temp[i];
+            sz++;
+            for(; i < temp.length && temp[i] == curr; i++)
+                ;
+        }
+        inv = new int[sz];
+        sz = 0;
+        for(int i = 0; i < temp.length; ) {
+            int curr = temp[i];
+            inv[sz++] = curr;
+            for(; i < temp.length && temp[i] == curr; i++)
+                ;
+        }
+        for(int i = 1; i < wt.length; i++)
+            wt[i] = Arrays.binarySearch(inv, wt[i]);
     }
     static class SegmentTree  { // Implemented to store min in a range , point update and range query
         int size[];
@@ -55,44 +59,49 @@ public class MathisonAndThePeculiarSums {
             odd  = new int[treeSize];
             even = new int[treeSize];
         }
-        void update(int node,int idx,int val,int nl,int nr) {
-            if(nl == nr && nl == idx) {
-                if(val == 1) {
-                    if(size[node] % 2 == 0)
-                        even[node] = (even[node] + inv[idx]) % mod;
-                    else
-                        odd[node] = (odd[node] + inv[idx]) % mod;
-                }
-                else {
-                    if(size[node] % 2 == 1)
-                        even[node] = (even[node] - inv[idx] + mod) % mod;
-                    else 
-                        odd[node] = (odd[node] - inv[idx] + mod) % mod;
-                }
-                size[node] += val;
-                // println("nl " + nl + " nr " + nr + " size " + size[node] + " ev " + even[node] + " od " + odd[node]);
+        void update(int idx , int val){
+            int node = (treeSize >> 1) + idx;
+            if(val == 1) {
+                if(size[node] % 2 == 0)
+                    even[node] = (even[node] + inv[idx]);
+                else
+                    odd[node] = (odd[node] + inv[idx]);
             }
             else {
-                int mid = (nl + nr) >> 1;
-                if(idx <= mid)
-                    update(2*node, idx , val ,nl , mid);
-                else
-                    update((2*node) + 1, idx ,val , mid + 1, nr);
-
+                if(size[node] % 2 == 1)
+                    even[node] = (even[node] - inv[idx]);
+                else 
+                    odd[node] = (odd[node] - inv[idx]);
+            }
+            if(even[node] >= mod)
+                even[node] -= mod;
+            if(even[node] < 0)
+                even[node] += mod;
+            if(odd[node] >= mod)
+                odd[node] -= mod;
+            if(odd[node] < 0)
+                odd[node] += mod;
+            
+            size[node] += val;
+            for(node >>= 1; node > 0; node >>= 1) {
                 size[node] = size[2 * node] + size[2 * node + 1];
                 if(size[2 * node] % 2 == 0) {
-                    even[node] = (even[2 * node] + even[2 * node + 1]) % mod;
-                    odd[node] = (odd[2 * node] + odd[2 * node + 1]) % mod;
+                    even[node] = (even[2 * node] + even[2 * node + 1]);
+                    odd[node] = (odd[2 * node] + odd[2 * node + 1]);
                 }
                 else {
-                    even[node] = (even[2 * node] + odd[2 * node + 1]) % mod;
-                    odd[node] = (odd[2 * node] + even[2 * node + 1]) % mod;
+                    even[node] = (even[2 * node] + odd[2 * node + 1]);
+                    odd[node] = (odd[2 * node] + even[2 * node + 1]);
                 }
-                // println("nl " + nl + " nr " + nr + " size " + size[node] + " ev " + even[node] + " od " + odd[node]); 
+                if(even[node] >= mod)
+                    even[node] -= mod;
+                if(even[node] < 0)
+                    even[node] += mod;
+                if(odd[node] >= mod)
+                    odd[node] -= mod;
+                if(odd[node] < 0)
+                    odd[node] += mod;
             }
-        }
-        void update(int idx , int val){
-            update(1, idx, val, 0, len - 1);
         }
         int query(){
             long evenSum = even[1];
@@ -106,32 +115,26 @@ public class MathisonAndThePeculiarSums {
     static int ans[];
     
     static void dfs(int u , int par) {
-        // println("Entering " + u);
         segTree.update(wt[u], 1);
         ans[u] = segTree.query();
         for(int v : adj[u])
             if(v != par)
                 dfs(v, u);
-        // println("Exiting " + u);
         segTree.update(wt[u], -1);
     }
     
-    @SuppressWarnings("unchecked")
     private static void solve() {
-        
         int N = nextInt();
         wt  = nextIntArrayOneBased(N);
         compress();
-        adj = new ArrayList[N + 1];
-        for(int i = 1; i <= N; i++)
-            adj[i] = new ArrayList<>();
         int E = N - 1;
+        int from[] = new int[E];
+        int to[] = new int[E];
         while(E-->0) {
-            int u = nextInt();
-            int v = nextInt();
-            adj[u].add(v);
-            adj[v].add(u);
+            from[E] = nextInt();
+            to[E] = nextInt();
         }
+        adj = packU(N, from, to, 1);
         int M = inv.length;
         segTree = new SegmentTree(M);
         ans = new int[N + 1];
