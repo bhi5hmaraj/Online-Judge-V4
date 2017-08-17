410f18447a23169ed9babb00f0c45205edece295
bhishmaraj <matib275@gmail.com>
2017-08-17 23:58:02 +0530

    commit at 17/08/2017 23:58:02 IST

---
+++ src/uva_11069.java
@@ -0,0 +1,81 @@
+import java.util.*;
+import java.io.*;
+public class uva_11069 {
+    
+    
+    
+    /************************ SOLUTION STARTS HERE ************************/
+    
+    
+    private static void solve() {
+        
+        String line;
+        int MAX = 76;
+        int F[][] = new int[MAX + 1][2]; // 0 - not taken , 1 - taken
+        
+        // base case 
+        F[1][0] = 0;
+        F[1][1] = 1;
+        F[2][0] = F[2][1] = 1;
+        
+        for(int i = 3; i <= MAX; i++) {
+            F[i][0] = F[i - 1][1];  // If you dont take ith node then you'll have to take i-1th node
+            F[i][1] = F[i - 2][1] + F[i - 2][0];    // if you take ith node then you can either take i-2 or not
+        }
+
+        while((line = nextLine()) != null) {
+            int n = Integer.parseInt(line);
+            println(F[n][0] + F[n][1]);
+        }
+        
+        // Just to check runtime !! 
+        double arr[][] = new double[500][500];
+        for(int i = 0; i < 500; i++)
+            for(int j = 0; j < 500; j++)
+                for(int k = 0; k < 500; k++)
+                    arr[i][k] += (1 + arr[i][j]) * (1 + arr[j][k]);
+            
+        
+    }
+    
+    
+    
+    /************************ SOLUTION ENDS HERE ************************/
+    
+    
+    
+    
+    
+    /************************ TEMPLATE STARTS HERE **********************/
+    
+    public static void main(String[] args) throws IOException {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)), false);
+        st     = null;
+        solve();
+        reader.close();
+        writer.close();
+    }
+    
+    static BufferedReader reader;
+    static PrintWriter    writer;
+    static StringTokenizer st;
+    
+    static String next()
+    {while(st == null || !st.hasMoreTokens()){try{String line = reader.readLine();if(line == null){return null;}            
+    st = new StringTokenizer(line);}catch (Exception e){throw new RuntimeException();}}return st.nextToken();}
+    static String nextLine()  {String s=null;try{s=reader.readLine();}catch(IOException e){e.printStackTrace();}return s;}             
+    static int    nextInt()   {return Integer.parseInt(next());}
+    static long   nextLong()  {return Long.parseLong(next());}     
+    static double nextDouble(){return Double.parseDouble(next());}
+    static char   nextChar()  {return next().charAt(0);}
+    static int[]  nextIntArray(int n)         {int[] a= new int[n];   int i=0;while(i<n){a[i++]=nextInt();}  return a;}
+    static long[] nextLongArray(int n)        {long[]a= new long[n];  int i=0;while(i<n){a[i++]=nextLong();} return a;}    
+    static int[]  nextIntArrayOneBased(int n) {int[] a= new int[n+1]; int i=1;while(i<=n){a[i++]=nextInt();} return a;}            
+    static long[] nextLongArrayOneBased(int n){long[]a= new long[n+1];int i=1;while(i<=n){a[i++]=nextLong();}return a;}            
+    static void   print(Object o)  { writer.print(o);  }
+    static void   println(Object o){ writer.println(o);}
+    
+    /************************ TEMPLATE ENDS HERE ************************/
+    
+}
