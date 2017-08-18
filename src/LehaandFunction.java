d490fb98eb271b883a4c2596c1863e833d555b0b
bhishmaraj <matib275@gmail.com>
2017-08-18 21:58:01 +0530

    commit at 18/08/2017 21:58:01 IST

---
+++ src/LehaandFunction.java
@@ -0,0 +1,85 @@
+import java.util.*;
+import java.io.*;
+public class LehaandFunction {
+    
+    
+    
+    /************************ SOLUTION STARTS HERE ************************/
+    
+    static void shuffleArray(int[] array) {
+        Random random = new Random();
+        for (int i = array.length - 1; i > 0; i--) {
+            int index = random.nextInt(i + 1);
+            int temp = array[index];
+            array[index] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    
+    private static void solve() {
+        
+        int n = nextInt();
+        int A[] = nextIntArray(n);
+        int B[][] = new int[n][2];
+        for(int i = 0; i < n; i++) {
+            B[i][0] = nextInt();
+            B[i][1] = i;
+        }
+            
+        
+        /*
+         * I dont have the proof , but I have to trust my intuition 
+         */
+        
+        shuffleArray(A);
+        Arrays.sort(A);
+        Arrays.sort(B, (a1 , a2) -> a1[0] - a2[0]);
+        int ans[] = new int[n];
+        for(int i = 0; i < n; i++)
+            ans[B[i][1]] = A[n - i - 1];
+        
+        Arrays.stream(ans).forEach(a -> print(a + " "));
+    }
+    
+    
+    
+    /************************ SOLUTION ENDS HERE ************************/
+    
+    
+    
+    
+    
+    /************************ TEMPLATE STARTS HERE **********************/
+    
+    public static void main(String[] args) throws IOException {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)), false);
+        st     = null;
+        solve();
+        reader.close();
+        writer.close();
+    }
+    
+    static BufferedReader reader;
+    static PrintWriter    writer;
+    static StringTokenizer st;
+    
+    static String next()
+    {while(st == null || !st.hasMoreTokens()){try{String line = reader.readLine();if(line == null){return null;}            
+    st = new StringTokenizer(line);}catch (Exception e){throw new RuntimeException();}}return st.nextToken();}
+    static String nextLine()  {String s=null;try{s=reader.readLine();}catch(IOException e){e.printStackTrace();}return s;}             
+    static int    nextInt()   {return Integer.parseInt(next());}
+    static long   nextLong()  {return Long.parseLong(next());}     
+    static double nextDouble(){return Double.parseDouble(next());}
+    static char   nextChar()  {return next().charAt(0);}
+    static int[]  nextIntArray(int n)         {int[] a= new int[n];   int i=0;while(i<n){a[i++]=nextInt();}  return a;}
+    static long[] nextLongArray(int n)        {long[]a= new long[n];  int i=0;while(i<n){a[i++]=nextLong();} return a;}    
+    static int[]  nextIntArrayOneBased(int n) {int[] a= new int[n+1]; int i=1;while(i<=n){a[i++]=nextInt();} return a;}            
+    static long[] nextLongArrayOneBased(int n){long[]a= new long[n+1];int i=1;while(i<=n){a[i++]=nextLong();}return a;}            
+    static void   print(Object o)  { writer.print(o);  }
+    static void   println(Object o){ writer.println(o);}
+    
+    /************************ TEMPLATE ENDS HERE ************************/
+    
+}
