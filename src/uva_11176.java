71001440832a4a377dc4f08635bebfba6b62a68c
bhishmaraj <matib275@gmail.com>
2017-08-18 16:58:01 +0530

    commit at 18/08/2017 16:58:01 IST

--- src/uva_11176.java
+++ src/uva_11176.java
@@ -6,38 +6,44 @@ public class uva_11176 {
     
     /************************ SOLUTION STARTS HERE ************************/
     
-    static double memo[][][][];
+    static double memo[][][];
     static double p;
-    static double rec(int idx , int streakRem , int done , int maxStreak) {
+    static double rec(int idx , int streakRem , int maxStreak) {
         if(idx == 0)
-            return (done == 1 || streakRem == 0) ? 1 : 0;
-        else if(memo[idx][streakRem][maxStreak][done] != -1)
-            return memo[idx][streakRem][maxStreak][done];
+            return 1;
+        else if(memo[idx][streakRem][maxStreak] != -1)
+            return memo[idx][streakRem][maxStreak];
         else if(streakRem == 0)
-            return (1 - p) * rec(idx - 1, maxStreak, 1 , maxStreak);
+            return (1 - p) * rec(idx - 1, maxStreak, maxStreak);
         else 
-            return memo[idx][streakRem][maxStreak][done] = p * rec(idx - 1, streakRem - 1, done , maxStreak) 
-                                        + (1 - p) * rec(idx - 1, maxStreak, done , maxStreak);
+            return memo[idx][streakRem][maxStreak] = p * rec(idx - 1, streakRem - 1, maxStreak) 
+                                        + (1 - p) * rec(idx - 1, maxStreak, maxStreak);
     }
     
     
     private static void solve() {
         
         int n;
+        double t = 0;
         while((n = nextInt()) != 0) {
             p = nextDouble();
+            long st = System.nanoTime();
             double expect = 0;
-            memo = new double[n + 1][n + 1][n + 1][2];
-            for(double a[][][] : memo)
-                for(double b[][] : a)
-                    for(double c[] : b)
-                        Arrays.fill(c, -1);
-            for(int i = 1; i <= n; i++) 
-                expect += 1.0 * i * rec(n, i, 0, i);
-            
-            println(String.format("%.6f", expect));
+            memo = new double[n + 1][n + 1][n + 1];
+            for(double b[][] : memo)
+                for(double c[] : b)
+                    Arrays.fill(c, -1);
+            double prev = rec(n, 0, 0);
+            for(int i = 1; i <= n; i++) { 
+                double curr = rec(n, i, i);
+                expect += 1.0 * i * (curr - prev);
+                prev = curr;
+            }
+            println(String.format("%.10f", expect));
+            t += (System.nanoTime() - st) / 1e9;
         }
         
+        println("Time : " + t);
     }
     
     
