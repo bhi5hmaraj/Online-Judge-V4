a4ddeab8f5f7691576e170df2500143e48820a41
bhishmaraj <matib275@gmail.com>
2017-08-21 23:58:01 +0530

    commit at 21/08/2017 23:58:01 IST

--- src/OBTTRNGL.java
+++ src/OBTTRNGL.java
@@ -6,95 +6,7 @@ class OBTTRNGL  {
     
     /************************ SOLUTION STARTS HERE ************************/
     
-    static class Vector {
-        double         x, y;
-        private double norm;
-        
 
-        public String toString() {
-            return String.format("(%.3f , %.3f)", x , y);
-        }
-        
-        Vector(double x, double y) {
-            this.x = x;
-            this.y = y;
-            this.norm = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-        }
-        
-        Vector() {
-            this(0, 0);
-        }
-
-        double magnitude() {
-            return norm;
-        }
-        
-        double angleTo(Vector that) {
-            double cosTheta = ((this.x * that.x) + (this.y * that.y)) / (this.magnitude() * that.magnitude());
-            return Math.acos(cosTheta);
-        }
-        
-        Vector add(Vector that) {
-            return new Vector(this.x + that.x, this.y + that.y);
-        }
-        
-        static Vector[] getPerpendicularUnit(Vector v) {
-            Vector[] vecs = new Vector[2];
-            double constant;
-            if(v.x == 0) { 
-                constant = 1.0 / Math.sqrt(1.0 + ((v.x * v.x) / (v.y * v.y)));
-                vecs[0] = new Vector(constant, -constant * (v.x / v.y));
-            }
-            else {
-                constant = 1.0 / Math.sqrt(1.0 + ((v.y * v.y) / (v.x * v.x)));
-                vecs[0] = new Vector(-constant * (v.y / v.x), constant);
-            }
-            vecs[1] = new Vector(-vecs[0].x, -vecs[0].y);
-            
-            return vecs;
-        }
-        
-        void multiply(double scalar) {
-            x *= scalar;
-            y *= scalar;
-            norm *= scalar; 
-        }
-        
-    }
-    static double pt[][];
-    static boolean isObtuse(int i , int a , int b) {
-        Vector va = new Vector(pt[a][0] - pt[i][0], pt[a][1] - pt[i][1]);
-        Vector vb = new Vector(pt[b][0] - pt[i][0], pt[b][1] - pt[i][1]);
-        return compare(va.angleTo(vb), Math.PI / 2.0) == 1;
-    }
-    
-    static int brute(int n , int a , int b) {
-        double radius = 100;
-        pt = new double[n][2];
-        for (int i = 0; i < n; i++) {
-            double angle = i * 2 * (Math.PI / (double)n);
-            pt[i][0] = radius * Math.cos(angle);
-            pt[i][1] = radius * Math.sin(angle);
-        }
-        int cnt = 0;
-        for(int i = 0; i < n; i++)
-            if(i != a && i != b) {
-                cnt += isObtuse(i, a, b) || isObtuse(a, i, b) || isObtuse(b, i, a) ? 1 : 0;
-            }
-        
-        //println("brute " + cnt);
-        return cnt;
-    }
-
-    static final double EPS = 1e-8;
-    static int compare(double a , double b) {
-        if(a <= b - EPS)
-            return -1;
-        else if(a >= b + EPS)
-            return 1;
-        else
-            return 0;
-    }
     private static void solve() {
         
         int T = nextInt();
@@ -108,14 +20,8 @@ class OBTTRNGL  {
             int b = Math.max(A , B);
             
             int minor = Math.min(b - a , k - (b - a));
-            long ans = 0;
-            if(!(k % 2 == 0 && b - a == k / 2)) {
-                ans += minor - 1;
-                ans += 1L * (k / 2 - minor - (k % 2 == 0 ? 1 : 0)) * 2;
-            }
-             println(ans);
-//             brute(k, A - 1, B - 1);
-            // println(brute(k, A - 1, B - 1));
+            println((k % 2 == 0 && (b - a) == k / 2) ? 0 : minor - 1);
+            
         }
         
     }
