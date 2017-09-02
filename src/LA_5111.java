0061dd3780588b48fd67246164b638591871e576
bhishmaraj <matib275@gmail.com>
2017-09-02 15:58:01 +0530

    commit at 02/09/2017 15:58:01 IST

--- src/LA_5111.java
+++ src/LA_5111.java
@@ -6,12 +6,48 @@ public class LA_5111 {
     
     /************************ SOLUTION STARTS HERE ************************/
     
-    
+    static int digits[];
+    static int total;
+    static Boolean memo[][][];
+    static boolean rec(int idx , int k , int sum) {
+        //System.out.println("idx " + idx + " k " + k + " sum " + sum);
+        if(k < 0)
+            return false;
+        if(idx < 0)
+            return k == 0 && sum == 0; 
+        else if(memo[idx][k][sum] != null)
+            return memo[idx][k][sum];
+        else 
+            return memo[idx][k][sum] = rec(idx - 1, k - 1, (sum - digits[idx] + 11) % 11) | rec(idx - 1, k, sum);
+    }
     private static void solve() {
         
         
         int T = nextInt();
-        
+        while(T-->0) {
+            
+            int d[] = nextIntArray(9);
+            int len = 0;
+            total = 0;
+            for(int a : d) len += a;
+            digits = new int[len];
+            int ptr = 0;
+            for(int i = 0; i < d.length; i++) {
+                total += (i + 1) * d[i];
+                while(d[i]-->0)
+                    digits[ptr++] = i + 1;
+            }
+            
+            memo = new Boolean[digits.length][digits.length + 1][11];
+            int min = Integer.MAX_VALUE;
+            for(int i = 1; i <= digits.length; i++)
+                if(rec(digits.length - 1, i, (total * 6) % 11)) {
+                    int need = Math.max(0 , Math.max(i , len - i) - Math.min(i , len - i) - 1);
+                    min = Math.min(min , need + len);
+                }
+            
+            println(min == Integer.MAX_VALUE ? -1 : min);
+        }
         
         
         
@@ -27,7 +63,7 @@ public class LA_5111 {
     
     /************************ TEMPLATE STARTS HERE **********************/
     
-    public static void main(String[] args) throws IOException {
+/*    public static void main(String[] args) throws IOException {
         reader = new BufferedReader(new InputStreamReader(System.in));
         writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)), false);
         st     = null;
@@ -35,6 +71,30 @@ public class LA_5111 {
         reader.close();
         writer.close();
     }
+    */
+    public static void main(String[] args) throws IOException {
+        new Thread(null, new Runnable() {
+            public void run() {
+                new LA_5111().run();
+            }
+        }, "Increase Stack", 1 << 25).start();
+
+    }
+
+    void run(){ 
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)), false);
+        st     = null;
+        solve();
+        try {
+            reader.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        writer.close();
+    }
+    
     
     static BufferedReader reader;
     static PrintWriter    writer;
