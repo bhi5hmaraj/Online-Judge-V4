0d1069e72ec052d5ac1715475069ad7b1cfb8918
bhishmaraj <matib275@gmail.com>
2017-09-04 23:30:01 +0530

    commit at 04/09/2017 23:30:01 IST

--- src/RedGreenTowers.java
+++ src/RedGreenTowers.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.IntStream;
 import java.io.*;
 public class RedGreenTowers {
     
@@ -6,28 +7,50 @@ public class RedGreenTowers {
     
     /************************ SOLUTION STARTS HERE ************************/
     
-    
-    static int maxDepth(int h , int R , int G /*, String str*/) {
-        if(R < h + 1 && G < h + 1) 
-            return h;
-        else {
-            int opt1 = 0 , opt2 = 0;
-            if(R >= h + 1)
-                opt1 = maxDepth(h + 1, R - h - 1, G/*, str + "R"*/);
-            if(G >= h + 1)
-                opt2 = maxDepth(h + 1, R, G - h - 1/*, str + "G"*/);
-            return Math.max(opt1 , opt2);
-        }
+    static final int mod = (int) 1e9 + 7;
+    static void pa(int a[]) {
+        Arrays.stream(a).forEach(i -> print(String.format("%3d ", i)));
+        print('\n');
+        IntStream.range(0, a.length).forEach(i -> print(String.format("%3d ", i)));
+        print('\n');
     }
-    
     private static void solve() {
-        
         
         int R = nextInt();
         int G = nextInt();
         
-        println(maxDepth(0, R, G /*, ""*/));
+        int DPprev[] = new int[R + 1];  // number of ways of building blocks using exactly i red blocks
+        boolean canPrev[] = new boolean[R + 1];
+        DPprev[0] = G > 0 ? 1 : 0;
+        DPprev[1] = R > 0 ? 1 : 0;
+        canPrev[0] = G > 0;
+        canPrev[1] = R > 0;
+        boolean flag = false;
+        int h = 2;
+        int sum = 0;
+        do {    // Upperbound = sqrt(R + G)
+            int DPnew[] = new int[R + 1];
+            boolean canNew[] = new boolean[R + 1];
+            flag = false;
+            for(int i = 0; i <= R; i++) {
+                int j = ((h * (h + 1)) / 2) - i;    // green
+                if(i >= h) {    // build base with red
+                    DPnew[i] = DPprev[i - h];
+                    canNew[i] |= canPrev[i - h];
+                }
+                if(j >= h && j <= G) {    // build base with green
+                    DPnew[i] = (DPnew[i] + DPprev[i]) % mod;
+                    canNew[i] |= canPrev[i];
+                }
+            }
+            for(boolean b : canNew) flag |= b;
+            sum = Arrays.stream(DPprev).reduce(0, (a , b) -> (a + b) % mod);
+            h++;
+            canPrev = canNew;
+            DPprev = DPnew;
+        }while(flag);
         
+        println(sum);
         
     }
     
