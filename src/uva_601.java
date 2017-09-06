296b4864b993343f2ec10fd3ccc9ba1696fc36c4
bhishmaraj <matib275@gmail.com>
2017-09-06 15:18:48 +0530

    commit at 06/09/2017 15:18:48 IST

--- src/uva_601.java
+++ src/uva_601.java
@@ -33,29 +33,62 @@ public class uva_601 {
             for(int i = 0; i < N; i++)
                 grid[i] = nextLine().toCharArray();
             nextLine(); // burn
-            boolean whiteMark[][] = new boolean[N][N];
-            for(int i = 0; i < N; i++)
-                if(grid[i][0] == 'W' && !whiteMark[i][0])
-                    dfs(i, 0, 'W', whiteMark);
-            boolean blackMark[][] = new boolean[N][N];
-            for(int i = 0; i < N; i++)
-                if(grid[0][i] == 'B' && !blackMark[0][i])
-                    dfs(0, i, 'B', blackMark);
+            boolean whiteMarkStart[][] = new boolean[N][N];
+            boolean whiteMarkEnd[][] = new boolean[N][N];
             
+            for(int i = 0; i < N; i++) {
+                if(grid[i][0] == 'W' && !whiteMarkStart[i][0])
+                    dfs(i, 0, 'W', whiteMarkStart);
+                if(grid[i][N - 1] == 'W' && !whiteMarkEnd[i][N - 1])
+                    dfs(i, N - 1, 'W', whiteMarkEnd);
+            }
+            
+            boolean blackMarkStart[][] = new boolean[N][N];
+            boolean blackMarkEnd[][] = new boolean[N][N];
+            for(int i = 0; i < N; i++) {
+                if(grid[0][i] == 'B' && !blackMarkStart[0][i])
+                    dfs(0, i, 'B', blackMarkStart);
+                if(grid[N - 1][i] == 'B' && !blackMarkEnd[N - 1][i])
+                    dfs(N - 1, i, 'B', blackMarkEnd);
+            }
+
             boolean whiteWin = false;
             boolean whiteWinnext = false;
+            boolean blackWinNext = false;
+
             for(int i = 0; i < N; i++) {
-                whiteWin |= whiteMark[i][N - 1];
-                if(N > 1)
-                    whiteWinnext |= (whiteMark[i][N - 2] && grid[i][N - 1] == 'U');
+                whiteWin |= whiteMarkStart[i][N - 1];
+                if(N > 1) {
+                    whiteWinnext |= whiteMarkStart[i][N - 2] && grid[i][N - 1] == 'U';
+                    whiteWinnext |= whiteMarkEnd[i][1] && grid[i][0] == 'U';
+                }
             }
             boolean blackWin = false;
-            boolean blackWinNext = false;
-            for(int i = 0; i < N; i++) {
-                blackWin |= blackMark[N - 1][i];
-                if(N > 1)
-                    blackWinNext |= blackMark[N - 2][i] && grid[N - 1][i] == 'U';
+            for(int i = 0; i < N; i++) { 
+                blackWin |= blackMarkStart[N - 1][i];
+                if(N > 1) {
+                    blackWinNext |= blackMarkStart[N - 2][i] && grid[N - 1][i] == 'U';
+                    blackWinNext |= blackMarkEnd[1][i] && grid[0][i] == 'U';
+                }
             }
+            
+            for(int i = 0; i < N; i++)
+                for(int j = 0; j < N; j++)
+                    if(grid[i][j] == 'U') 
+                        for(int k = 0; k < 4; k++)
+                            for(int l = 0; l < 4; l++) 
+                                if(k != l) {
+                                    int x1 = i + x[k];
+                                    int y1 = j + y[k];
+                                    int x2 = i + x[l];
+                                    int y2 = j + y[l];
+                                    whiteWinnext |= (isValid(x1, y1) && whiteMarkStart[x1][y1]) &&
+                                                    (isValid(x2, y2) && whiteMarkEnd[x2][y2]);
+                                    blackWinNext |= (isValid(x1, y1) && blackMarkStart[x1][y1]) &&
+                                                    (isValid(x2, y2) && blackMarkEnd[x2][y2]);
+                                }
+                        
+                    
             
             if(whiteWin)          println("White has a winning path.");
             else if(blackWin)     println("Black has a winning path.");
