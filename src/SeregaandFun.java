f265adb995f1f5724d087f5aa9a7564a2b095fee
bhishmaraj <matib275@gmail.com>
2017-09-27 17:30:01 +0530

    commit at 27/09/2017 17:30:01 IST

--- src/SeregaandFun.java
+++ src/SeregaandFun.java
@@ -10,10 +10,49 @@ public class SeregaandFun {
     private static void solve() {
         
         
+        int n = nextInt();
+        int arr[] = nextIntArray(n);
+        int Q = nextInt();
+
+        final int BLOCK_SIZE = (int) (Math.sqrt(n) + 1); 
+        LinkedList<Integer> blocks[] = new LinkedList[n / BLOCK_SIZE + 1];
+        HashMap<Integer , Integer> freq[] = new HashMap[n / BLOCK_SIZE + 1];
         
+        for(int i = 0; i < blocks.length; i++) {
+            blocks[i] = new LinkedList<>();
+            freq[i] = new HashMap<>();
+        }
+        for(int i = 0; i < n; i++) {
+            blocks[i / BLOCK_SIZE].add(arr[i]);
+            freq[i / BLOCK_SIZE].put(arr[i], freq[i / BLOCK_SIZE].getOrDefault(arr[i] , 0) + 1);
+        }
         
-        
-        
+        int ans = 0;
+        while(Q-->0) {
+            int type = nextInt();
+            if(type == 1) {
+                int L = ((nextInt() + ans - 1) % n);
+                int R = ((nextInt() + ans - 1) % n);
+                boolean add = false;
+                for(int i = L / BLOCK_SIZE; i < R / BLOCK_SIZE; i++) {
+                    add = true;
+                    int toRem = blocks[i].pollLast();
+                    freq[i].put(toRem, freq[i].get(toRem) - 1);
+                    if(freq[i].get(toRem).intValue() == 0)
+                        freq[i].remove(toRem);
+                    blocks[i + 1].addFirst(toRem);
+                    freq[i + 1].put(toRem, freq[i + 1].getOrDefault(toRem, 0) + 1);
+                }
+                
+                int toRem = blocks[R / BLOCK_SIZE].remove((R % BLOCK_SIZE) + (add ? 1 : 0));
+                freq[R / BLOCK_SIZE].put(toRem, freq[R / BLOCK_SIZE].get(toRem) - 1);
+                if(freq[R / BLOCK_SIZE].get(toRem).intValue() == 0)
+                    freq[R / BLOCK_SIZE].remove(toRem);
+                blocks[L / BLOCK_SIZE].add(L % BLOCK_SIZE, toRem);
+                freq[L / BLOCK_SIZE].put(toRem, freq[L / BLOCK_SIZE].getOrDefault(toRem, 0) + 1);
+            }
+        }
+            
     }
     
     
