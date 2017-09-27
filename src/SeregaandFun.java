c4ab30eb73bd56394d0bb40b1d1820bf53e3facf
bhishmaraj <matib275@gmail.com>
2017-09-27 19:30:01 +0530

    commit at 27/09/2017 19:30:01 IST

--- src/SeregaandFun.java
+++ src/SeregaandFun.java
@@ -18,6 +18,7 @@ public class SeregaandFun {
         LinkedList<Integer> blocks[] = new LinkedList[n / BLOCK_SIZE + 1];
         HashMap<Integer , Integer> freq[] = new HashMap[n / BLOCK_SIZE + 1];
         
+        long start = System.nanoTime();
         for(int i = 0; i < blocks.length; i++) {
             blocks[i] = new LinkedList<>();
             freq[i] = new HashMap<>();
@@ -26,18 +27,25 @@ public class SeregaandFun {
             blocks[i / BLOCK_SIZE].add(arr[i]);
             freq[i / BLOCK_SIZE].put(arr[i], freq[i / BLOCK_SIZE].getOrDefault(arr[i] , 0) + 1);
         }
+        long stop = System.nanoTime();
+        if(arr[0] == 65899)
+            System.out.println("Time : " + (stop - start) / 1e9);
+        /*
         System.out.println("Block sz = " + BLOCK_SIZE);
         for(int i = 0; i < blocks.length; i++) {
             println("i " + i);
             println(blocks[i]);
             println(freq[i]);
         }
+        */
         int ans = 0;
         while(Q-->0) {
             int type = nextInt();
             if(type == 1) {
-                int L = ((nextInt() + ans - 1) % n);
-                int R = ((nextInt() + ans - 1) % n);
+                int l = ((nextInt() + ans - 1) % n);
+                int r = ((nextInt() + ans - 1) % n);
+                int L = Math.min(l , r);
+                int R = Math.max(l , r);
                 boolean add = false;
                 for(int i = L / BLOCK_SIZE; i < R / BLOCK_SIZE; i++) {
                     add = true;
@@ -50,28 +58,24 @@ public class SeregaandFun {
                 }
                 
                 int toRem = blocks[R / BLOCK_SIZE].remove((R % BLOCK_SIZE) + (add ? 1 : 0));
-                freq[R / BLOCK_SIZE].put(toRem, freq[R / BLOCK_SIZE].get(toRem) - 1);
-                if(freq[R / BLOCK_SIZE].get(toRem).intValue() == 0)
-                    freq[R / BLOCK_SIZE].remove(toRem);
+                int f = freq[R / BLOCK_SIZE].get(toRem);
+                
+                if(f > 1) freq[R / BLOCK_SIZE].remove(toRem);
+                else freq[R / BLOCK_SIZE].put(toRem, f - 1);
+
                 blocks[L / BLOCK_SIZE].add(L % BLOCK_SIZE, toRem);
                 freq[L / BLOCK_SIZE].put(toRem, freq[L / BLOCK_SIZE].getOrDefault(toRem, 0) + 1);
                 
-                for(int i = 0; i < blocks.length; i++) {
-                    println("i " + i);
-                    println(blocks[i]);
-                    println(freq[i]);
-                }
-                
             } else {
-                int L = ((nextInt() + ans - 1) % n);
-                int R = ((nextInt() + ans - 1) % n);
-                //System.out.println(blocks[R / BLOCK_SIZE]);
-                //System.out.println(blocks[R / BLOCK_SIZE].listIterator(R % BLOCK_SIZE + 1).previous());
+                int l = ((nextInt() + ans - 1) % n);
+                int r = ((nextInt() + ans - 1) % n);
+                int L = Math.min(l , r);
+                int R = Math.max(l , r);
                 int K = ((nextInt() + ans - 1) % n) + 1;
                 int cnt = 0;
                 for(ListIterator<Integer> it =  blocks[L / BLOCK_SIZE].listIterator(L % BLOCK_SIZE); L <= R && L % BLOCK_SIZE != 0; L++)
                     cnt += it.next().intValue() == K ? 1 : 0;
-                for(ListIterator<Integer> it = blocks[R / BLOCK_SIZE].listIterator(R % BLOCK_SIZE + 1); R >= L && (R + BLOCK_SIZE) % BLOCK_SIZE != BLOCK_SIZE - 1; R--)
+                for(ListIterator<Integer> it = blocks[R / BLOCK_SIZE].listIterator((R % BLOCK_SIZE) + 1); R >= L && (R + BLOCK_SIZE) % BLOCK_SIZE != BLOCK_SIZE - 1; R--)
                     cnt += it.previous().intValue() == K ? 1 : 0;
                 
                 for(; L < R; L += BLOCK_SIZE)
