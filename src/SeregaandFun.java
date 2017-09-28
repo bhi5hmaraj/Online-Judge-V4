d1584c6c9b2c6f21dc9af79b7ade6cb33b9a9766
bhishmaraj <matib275@gmail.com>
2017-09-28 12:30:01 +0530

    commit at 28/09/2017 12:30:01 IST

--- src/SeregaandFun.java
+++ src/SeregaandFun.java
@@ -16,15 +16,13 @@ public class SeregaandFun {
 
         final int BLOCK_SIZE = (int) (Math.sqrt(n) + 1); 
         LinkedList<Integer> blocks[] = new LinkedList[n / BLOCK_SIZE + 1];
-        HashMap<Integer , Integer> freq[] = new HashMap[n / BLOCK_SIZE + 1];
-        
-        for(int i = 0; i < blocks.length; i++) {
+        int freq[][] = new int[n / BLOCK_SIZE + 1][n + 1];
+        for(int i = 0; i < blocks.length; i++) 
             blocks[i] = new LinkedList<>();
-            freq[i] = new HashMap<>();
-        }
+        
         for(int i = 0; i < n; i++) {
             blocks[i / BLOCK_SIZE].add(arr[i]);
-            freq[i / BLOCK_SIZE].put(arr[i], freq[i / BLOCK_SIZE].getOrDefault(arr[i] , 0) + 1);
+            freq[i / BLOCK_SIZE][arr[i]]++;
         }
         /*
         System.out.println("Block sz = " + BLOCK_SIZE);
@@ -35,12 +33,9 @@ public class SeregaandFun {
         }
         */
         int ans = 0;
-        double q1T = 0;
-        double q2T = 0;
         while(Q-->0) {
             int type = nextInt();
             if(type == 1) {
-                long start = System.nanoTime();
                 int l = ((nextInt() + ans - 1) % n);
                 int r = ((nextInt() + ans - 1) % n);
                 int L = Math.min(l , r);
@@ -49,26 +44,16 @@ public class SeregaandFun {
                 for(int i = L / BLOCK_SIZE; i < R / BLOCK_SIZE; i++) {
                     add = true;
                     int toRem = blocks[i].pollLast();
-                    int f = freq[i].get(toRem);
-                    
-                    if(f == 1) freq[i].remove(toRem);
-                    else freq[i].put(toRem, f - 1);
-                    
+                    freq[i][toRem]--;
                     blocks[i + 1].addFirst(toRem);
-                    freq[i + 1].put(toRem, freq[i + 1].getOrDefault(toRem, 0) + 1);
+                    freq[i + 1][toRem]++;
                 }
                 
                 int toRem = blocks[R / BLOCK_SIZE].remove((R % BLOCK_SIZE) + (add ? 1 : 0));
-                int f = freq[R / BLOCK_SIZE].get(toRem);
-                
-                if(f == 1) freq[R / BLOCK_SIZE].remove(toRem);
-                else freq[R / BLOCK_SIZE].put(toRem, f - 1);
-
+                freq[R / BLOCK_SIZE][toRem]--;
                 blocks[L / BLOCK_SIZE].add(L % BLOCK_SIZE, toRem);
-                freq[L / BLOCK_SIZE].put(toRem, freq[L / BLOCK_SIZE].getOrDefault(toRem, 0) + 1);
-                q1T += (System.nanoTime() - start) / 1e9;
+                freq[L / BLOCK_SIZE][toRem]++;
             } else {
-                long start = System.nanoTime();
                 int l = ((nextInt() + ans - 1) % n);
                 int r = ((nextInt() + ans - 1) % n);
                 int L = Math.min(l , r);
@@ -81,15 +66,11 @@ public class SeregaandFun {
                     cnt += it.previous().intValue() == K ? 1 : 0;
                 
                 for(; L < R; L += BLOCK_SIZE)
-                    cnt += freq[L / BLOCK_SIZE].getOrDefault(K, 0);
+                    cnt += freq[L / BLOCK_SIZE][K];
                 
-                q2T += (System.nanoTime() - start) / 1e9;
                 println(ans = cnt);
             }
         }
-        
-        if(n == 100000)
-            println("Q1T " + q1T + " q2T " + q2T);
             
     }
     
@@ -104,13 +85,11 @@ public class SeregaandFun {
         int blocks[][] = new int[n / BLOCK_SIZE + 1][BLOCK_SIZE];
         int offset[] = new int[n / BLOCK_SIZE + 1];
         int aux[] = new int[BLOCK_SIZE];
-        HashMap<Integer , Integer> freq[] = new HashMap[n / BLOCK_SIZE + 1];
-        for(int i = 0; i < blocks.length; i++)
-            freq[i] = new HashMap<>();
+        int freq[][] = new int[n / BLOCK_SIZE + 1][n + 1];
         
         for(int i = 0; i < n; i++) {
             blocks[i / BLOCK_SIZE][i % BLOCK_SIZE] = arr[i];
-            freq[i / BLOCK_SIZE].put(arr[i], freq[i / BLOCK_SIZE].getOrDefault(arr[i], 0) + 1);
+            freq[i / BLOCK_SIZE][arr[i]]++;
         }
         
         int ans = 0;
@@ -144,34 +123,19 @@ public class SeregaandFun {
                     int re = L % BLOCK_SIZE;
                     exit = blocks[qu][BLOCK_SIZE - 1];
                     
-                    freq[qu].compute(exit, (k , v) -> v == 1 ? freq[qu].remove(k) : freq[qu].put(k, v - 1));
-                    /*
-                    int f = freq[qu].get(exit);
-                    
-                    if(f == 1) freq[qu].remove(exit);
-                    else freq[qu].put(exit, f - 1);
-                    */
+                    freq[qu][exit]--;
                     for(int i = BLOCK_SIZE - 1; i > re; i--)
                         blocks[qu][i] = blocks[qu][i - 1];
                     blocks[qu][re] = blocks[R / BLOCK_SIZE][(offset[R / BLOCK_SIZE] + R) % BLOCK_SIZE];
-                    freq[qu].put(blocks[qu][re], freq[qu].getOrDefault(blocks[qu][re], 0) + 1);
-                    
+                    freq[qu][blocks[qu][re]]++;
                     for(int i = L / BLOCK_SIZE + 1; i < R / BLOCK_SIZE; i++) {
                         offset[i] = (offset[i] - 1 + BLOCK_SIZE) % BLOCK_SIZE;
                         int next = blocks[i][offset[i]];
                         blocks[i][offset[i]] = exit;
-                        freq[i].put(exit, freq[i].getOrDefault(exit, 0) + 1);
+                        freq[i][exit]++;
                         exit = next;
+                        freq[i][exit]--;
                         
-                        final int wtf = i;
-                        freq[wtf].compute(exit, (k , v) -> v == 1 ? freq[wtf].remove(k) : freq[wtf].put(k , v - 1));
-                        
-                        /*
-                        f = freq[i].get(exit);
-                        
-                        if(f == 1) freq[i].remove(exit);
-                        else freq[i].put(exit, f - 1);
-                        */
                     }
                     
 
@@ -182,25 +146,15 @@ public class SeregaandFun {
                         offset[R / BLOCK_SIZE] = 0;
                     }
                     
-                    freq[R / BLOCK_SIZE].put(exit, freq[R / BLOCK_SIZE].getOrDefault(exit, 0) + 1);
-                    
+                    freq[R / BLOCK_SIZE][exit]++;
                     int toRem = blocks[R / BLOCK_SIZE][R % BLOCK_SIZE];
-                    freq[R / BLOCK_SIZE].compute(toRem, (k , v) -> v == 1 ? freq[R / BLOCK_SIZE].remove(k) : 
-                                                                            freq[R / BLOCK_SIZE].put(k , v - 1));
-                    
+                    freq[R / BLOCK_SIZE][toRem]--;
                     for(int i = R % BLOCK_SIZE; i > 0; i--)
                         blocks[R / BLOCK_SIZE][i] = blocks[R / BLOCK_SIZE][i - 1];
                     
                     blocks[R / BLOCK_SIZE][0] = exit;
                     
                 }
-                
-                for(int i = 0; i < blocks.length; i++) {
-                    System.out.println("i " + i);
-                    System.out.println("bl " + Arrays.toString(blocks[i]));
-                    System.out.println("freq " + freq[i]);
-                }
-                
             } else {
                 int l = ((nextInt() + ans - 1) % n);
                 int r = ((nextInt() + ans - 1) % n);
@@ -213,7 +167,7 @@ public class SeregaandFun {
                 for(int idx = R / BLOCK_SIZE; R >= L && (R + BLOCK_SIZE) % BLOCK_SIZE != BLOCK_SIZE - 1; R--)
                     cnt += blocks[idx][(offset[idx] + R) % BLOCK_SIZE] == K ? 1 : 0;
                 for(; L < R; L += BLOCK_SIZE)
-                    cnt += freq[L / BLOCK_SIZE].getOrDefault(K, 0);
+                    cnt += freq[L / BLOCK_SIZE][K];
                 
                 println(ans = cnt);
             }
@@ -233,6 +187,7 @@ public class SeregaandFun {
         writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)), false);
         st     = null;
         solve2();
+        // LinkedList nearly passed !! TC 55 TLE
         reader.close();
         writer.close();
     }
