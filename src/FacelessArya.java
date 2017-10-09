2761036bf6feec0767dd42195b97702eb420c726
bhishmaraj <matib275@gmail.com>
2017-10-09 18:31:26 +0530

    commit at 09/10/2017 18:31:26 IST

--- src/FacelessArya.java
+++ src/FacelessArya.java
@@ -6,11 +6,91 @@ public class FacelessArya {
     
     /************************ SOLUTION STARTS HERE ************************/
     
+
+    static int MAX = 1000000;
+    static int bigPrime[] = new int[MAX + 1];
     
+    // instead of the loPrimesieve you could use bigprimeSieve which has the same performance and its a lot more intutive
+    
+    static {
+        bigPrime[1] = 1;
+        for (int i = 2; i <= MAX; i++) {
+            if (bigPrime[i] == 0) {
+                bigPrime[i] = i;
+                for (int j = 2 * i; j <= MAX; j += i)
+                    bigPrime[j] = i;
+            }
+        }
+    }
     private static void solve() {
         
+        int next[] = new int[MAX + 1];
+        int prev[] = new int[MAX + 1];
         
-        
+        int T = nextInt();
+        while(T-->0) {
+            Arrays.fill(next, 0);
+            Arrays.fill(prev, 0);
+            int N = nextInt();
+            int M = nextInt();
+            int arr[][] = new int[N][];
+            for(int i = 0; i < N; i++)
+                arr[i] = nextIntArray(M);
+            
+            if(N == 1) {
+                println(Arrays.stream(arr[0]).max().getAsInt());
+                continue;
+            }
+            
+            for(int a : arr[0]) {
+                int cost = a;
+                while(a > 1) {
+                    int p = bigPrime[a];
+                    prev[p] = Math.max(prev[p] , cost);
+                    while(a % p == 0)
+                        a /= p;
+                }
+            }
+            
+            for(int i = 1; i < N; i++) {
+                for(int a : arr[i]) {
+                    int cost = a;
+                    int optPrev = 0;
+                    int temp = a;
+                    while(temp > 1) {
+                        int p = bigPrime[temp];
+                        optPrev = Math.max(optPrev , prev[p]);
+                        while(temp % p == 0)
+                            temp /= p;
+                    }
+                    
+                    if(optPrev > 0) {
+                        while(a > 1) {
+                            int p = bigPrime[a];
+                            next[p] = Math.max(next[p] , optPrev + cost);
+                            while(a % p == 0)
+                                a /= p;
+                        }
+                    }
+                }
+                
+                for(int a : arr[i - 1]) {
+                    while(a > 1) {
+                        int p = bigPrime[a];
+                        prev[p] = 0;
+                        while(a % p == 0)
+                            a /= p;
+                    }
+                }
+                
+                int swap[] = prev;
+                prev = next;
+                next = swap;
+                
+            }
+            
+            println(Arrays.stream(prev).max().getAsInt());
+        }
         
         
         
