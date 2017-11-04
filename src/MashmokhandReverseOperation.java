eb592d6ac2393b5737ec7788c7db83da0eb03cc3
bhishmaraj <matib275@gmail.com>
2017-11-04 11:30:01 +0530

    commit at 04/11/2017 11:30:01 IST

--- src/MashmokhandReverseOperation.java
+++ src/MashmokhandReverseOperation.java
@@ -6,8 +6,34 @@ public class MashmokhandReverseOperation {
     
     /************************ SOLUTION STARTS HERE ************************/
     
-    static void fill(int depth , int L , int R) {
+    static int aux[];
+    static long fill(int depth , int L , int R , int arr[] , long inv[]) {
+        if(L < R) {
+            int mid = (L + R) >> 1;
+            long left = fill(depth + 1, L, mid, arr, inv);
+            long right = fill(depth + 1, mid + 1, R, arr, inv);
+            System.arraycopy(arr, L, aux, L, R - L + 1);
+            long cross = 0;
+            System.out.println("L " + L + " R " + R + " arr " + Arrays.toString(Arrays.copyOfRange(arr, L, R + 1)));
+            for(int ptrL = L , ptrR = mid + 1 , ptr = L; ptrL <= mid || ptrR <= R; ) {
+                // System.out.println("ptrL " + ptrL + " ptrR " + ptrR);
+                if(ptrL > mid)
+                    arr[ptr++] = aux[ptrR++];
+                else if(ptrR > R)
+                    arr[ptr++] = aux[ptrL++];
+                else if(aux[ptrL] <= aux[ptrR]) 
+                    arr[ptr++] = aux[ptrL++];
+                else if(aux[ptrR] < aux[ptrL]) {
+                    arr[ptr++] = aux[ptrR++];
+                    cross += mid - ptrL + 1;
+                }
+            }
+            System.out.println("inv " + (left + right + cross));
+            inv[depth] += left + right + cross;
+            return left + right + cross;
+        }
         
+        return 0;
     }
     
     
@@ -17,13 +43,22 @@ public class MashmokhandReverseOperation {
         int n = nextInt();
         int m = 1 << n;
         int a[] = nextIntArray(m);
+        int b[] = new int[m];
+        for(int i = 0; i < m; i++)
+            b[i] = a[m - i - 1];
         
+        long inv[][] = new long[2][n];
+        aux = new int[m];
+        fill(0, 0, m - 1, a, inv[0]);
+        // fill(0, 0, m - 1, b, inv[1]);
+        println(Arrays.toString(inv[0]));
+        /*
         int q = nextInt();
         while(q-->0) {
             
             
             
-        }
+        }*/
     }
     
     
