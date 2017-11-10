5d86f4b8132005b93840b627d3c54c4985ccf196
bhishmaraj <matib275@gmail.com>
2017-11-11 03:30:02 +0530

    commit at 11/11/2017 03:30:02 IST

--- src/LA_5116.java
+++ src/LA_5116.java
@@ -18,22 +18,33 @@ public class LA_5116 {
             cardinality = 0;
         }
 
-        void set(int n, boolean f) {
+        void set(int n) {
             int index = n / 64;
             if((bits[index] & (1L << (n % 64))) == 0)
                 cardinality++;
             bits[index] |= (1L << (n % 64));
         }
 
-        void set(int n) {
-            set(n, true);
-        }
         
         int cardinality() {
             return cardinality;
         }
     }
+    
+    static int primes[] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67};
+    static MyBitSet bitset;
 
+    static void rec(int idx , int n , int val) {
+        if(idx < primes.length && n >= 0) {
+            bitset.set(val);
+            int curr = 1;
+            for(int i = 0; n - primes[idx] * i >= 0; i++) {
+                rec(idx + 1, n - primes[idx] * i, (int) ((1L * val * curr) % (long) P));
+                curr = (int) ((1L * curr * primes[idx]) % (long) P);
+            }
+        }
+    }
+    
     private static void solve() {
         
         
@@ -41,7 +52,10 @@ public class LA_5116 {
         while(T-->0) {
             int N = nextInt();
             P = nextInt();
-            
+            bitset = new MyBitSet(P);
+            rec(0, N, 1);
+            bitset.set(N % P);
+            println(bitset.cardinality());
         }
         
         
