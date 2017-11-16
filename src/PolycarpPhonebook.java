fd7bbda24e0553231456fe64be2451b4e11e19c0
bhishmaraj <matib275@gmail.com>
2017-11-16 22:30:01 +0530

    commit at 16/11/2017 22:30:01 IST

--- src/PolycarpPhonebook.java
+++ src/PolycarpPhonebook.java
@@ -6,6 +6,65 @@ public class PolycarpPhonebook {
     
     /************************ SOLUTION STARTS HERE ************************/
     
+
+    static class Trie {
+        static class Node {
+            int size;
+            Node adj[];
+            Node() {
+                size = 1;   // Take this into consideration
+                adj = new Node[10];
+            }
+        }
+        Node root;
+        Trie(){
+            root = new Node();
+        }
+
+        void insert(int num){
+            root = insert(root, num, offset);
+        }
+        
+        int size(Node n) {
+            return n == null ? 0 : n.size;
+        }
+        
+        Node insert(Node curr , int num , int idx){
+            if(idx < 0) {
+                if(curr != null)
+                    curr.size++;
+                else
+                    curr = new Node();
+                return curr;
+            }
+            else{
+                if(curr == null)
+                    curr = new Node();
+                if((num & (1 << idx)) == 0)
+                    curr.zero = insert(curr.zero, num, idx - 1);
+                else
+                    curr.one = insert(curr.one, num, idx - 1);
+                
+                curr.size = size(curr.zero) + size(curr.one);
+                return curr;
+            }
+        }
+        
+        void remove(int num){
+            remove(root, null, num, offset);
+        }
+        
+        void remove(Node curr , Node parent , int num , int idx){
+            if(idx >= 0){
+                if((num & (1 << idx)) == 0)
+                    remove(curr.zero, curr, num, idx - 1);
+                else
+                    remove(curr.one, curr, num, idx - 1);
+            }
+            curr.size--;
+        }
+    }
+    
     
     private static void solve() {
         
