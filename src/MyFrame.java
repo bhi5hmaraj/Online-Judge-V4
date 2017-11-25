3a7c5c97ad3a03533c5bbf7692b995b717c046d8
bhishmaraj <matib275@gmail.com>
2017-11-25 19:30:01 +0530

    commit at 25/11/2017 19:30:01 IST

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -23,61 +23,56 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.swing.JButton;
-public class MyFrame extends JFrame{
-    MyFrame()
-    {
-        MyPanel mp =new MyPanel();
+
+public class MyFrame extends JFrame {
+    MyFrame() {
+        MyPanel mp = new MyPanel();
         mp.addMouseListener(new MyMouse());
         mp.addMouseMotionListener(new MouseMotion());
         mp.add(lbl);
         this.add(mp);
         new Thread(mp).start();
     }
-    static boolean mousePressed = false,inside=false,mouseRel =true;
-    static JLabel lbl = new JLabel();
-    static int s_x,s_y,e_x,e_y;
-    public static void main(String args[])
-    {
+
+    static boolean mousePressed = false, inside = false, mouseRel = true;
+    static JLabel  lbl          = new JLabel();
+    static int     s_x, s_y, e_x, e_y;
+
+    public static void main(String args[]) {
         MyFrame f = new MyFrame();
-        //f.add(new JButton("abcd"));
+        // f.add(new JButton("abcd"));
         f.setSize(500, 500);
         f.setLocationRelativeTo(null);
-        //(200,200)        
+        // (200,200)
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
-    
+
     }
-    class MyPanel extends JPanel implements Runnable
-    {
-        protected void paintComponent(Graphics g)
-        {
+
+    class MyPanel extends JPanel implements Runnable {
+        protected void paintComponent(Graphics g) {
             super.paintComponents(g);
-            //g.drawString("Hello World", 10, 10);    
+            // g.drawString("Hello World", 10, 10);
             g.setColor(Color.WHITE);
-            g.fillRect(0, 0,getWidth(),getHeight());
+            g.fillRect(0, 0, getWidth(), getHeight());
             g.setColor(Color.BLACK);
             g.drawOval(100, 100, 200, 200);
-            //g.fillOval(100, 100, 150, 150);
-            if(inside)
-            {
+            // g.fillOval(100, 100, 150, 150);
+            if (inside) {
                 g.drawString("Inside circle", 50, 50);
                 lbl.setText("Inside circle");
-            }
-            else
-            {
+            } else {
                 g.drawString("Outside Circle", 50, 50);
                 lbl.setText("Outside");
             }
-            
-            if(!mouseRel)
-            {
-                g.fillOval(s_x-25, s_y-25, 50, 50);
+
+            if (!mouseRel) {
+                g.fillOval(s_x - 25, s_y - 25, 50, 50);
             }
         }
-        public void run()
-        {
-            while(true)
-            {
+
+        public void run() {
+            while (true) {
                 repaint();
                 try {
                     Thread.sleep(10);
@@ -87,52 +82,57 @@ public class MyFrame extends JFrame{
             }
         }
     }
-    
-    static class MyMouse implements MouseListener
-    {
+
+    static class MyMouse implements MouseListener {
 
         @Override
         public void mouseClicked(MouseEvent e) {
-            System.out.println("mouse clicked"); //To change body of generated methods, choose Tools | Templates.*/
+            System.out.println("mouse clicked"); // To change body of generated
+                                                 // methods, choose Tools |
+                                                 // Templates.*/
         }
 
         @Override
         public void mouseEntered(MouseEvent e) {
-        
+
         }
 
         @Override
         public void mouseExited(MouseEvent e) {
-            System.out.println("Mouse exited frame"); //To change body of generated methods, choose Tools | Templates.
+            System.out.println("Mouse exited frame"); // To change body of
+                                                      // generated methods,
+                                                      // choose Tools |
+                                                      // Templates.
         }
 
         @Override
         public void mousePressed(MouseEvent e) {
-            System.out.println("mouse pressed"); //To change body of generated methods, choose Tools | Templates.
+            System.out.println("mouse pressed"); // To change body of generated
+                                                 // methods, choose Tools |
+                                                 // Templates.
         }
 
         @Override
-        public void mouseReleased(MouseEvent e) 
-        {
-            mouseRel =true;
+        public void mouseReleased(MouseEvent e) {
+            mouseRel = true;
             mousePressed = false;
             e_x = e.getX();
             e_y = e.getY();
-            System.out.println("mouse released"); //To change body of generated methods, choose Tools | Templates.            
+            System.out.println("mouse released"); // To change body of generated
+                                                  // methods, choose Tools |
+                                                  // Templates.
         }
-        
-        
-        
+
     }
-    static class MouseMotion implements MouseMotionListener
-    {
+
+    static class MouseMotion implements MouseMotionListener {
 
         @Override
         public void mouseDragged(MouseEvent e) {
             int x = e.getX();
             int y = e.getY();
-            mousePressed=true;
-            mouseRel =false;
+            mousePressed = true;
+            mouseRel = false;
             s_x = x;
             s_y = y;
         }
@@ -141,18 +141,17 @@ public class MyFrame extends JFrame{
         public void mouseMoved(MouseEvent e) {
             int x = e.getX();
             int y = e.getY();
-            System.out.println("x="+x+"y="+y);
-            int c_x = 200;  //center of circle
+            System.out.println("x=" + x + "y=" + y);
+            int c_x = 200; // center of circle
             int c_y = 200;
             int radius = 100;
-            int dist = (int)Math.sqrt(Math.pow(x-c_x,2) + Math.pow(y-c_y,2));
-            System.out.println("Distance="+dist);   
-            if(dist<=radius)
-                inside =true;
+            int dist = (int) Math.sqrt(Math.pow(x - c_x, 2) + Math.pow(y - c_y, 2));
+            System.out.println("Distance=" + dist);
+            if (dist <= radius)
+                inside = true;
             else
-                inside =false;
+                inside = false;
         }
-        
+
     }
 }
- 
