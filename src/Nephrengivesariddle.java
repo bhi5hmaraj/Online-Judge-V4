df3cc8bfd141dff551d66fe8b9f181edc6a5a14b
bhishmaraj <matib275@gmail.com>
2017-12-02 21:30:01 +0530

    commit at 02/12/2017 21:30:01 IST

--- src/Nephrengivesariddle.java
+++ src/Nephrengivesariddle.java
@@ -10,16 +10,18 @@ public class Nephrengivesariddle {
     private static void solve() {
         
         String base = "What are you doing at the end of the world? Are you busy? Will you save us?";
-        String rep1 =  "What are you doing while sending \"";
-        String rep2 =  "\"? Are you busy? Will you send \"\"?";
-        
-        long lengths[] = new long[70];
+        String rep1 =  "What are you doing while sending \"";   // 34
+        String rep2 =  "\"? Are you busy? Will you send \"";
+        String rep3 =  "\"?";
+        long lengths[] = new long[60];
         lengths[0] = base.length();
         int sz = 1;
         while(lengths[sz - 1] <= (long) 1e18) {
-            lengths[sz] = 2L * lengths[sz - 1] + rep1.length() + rep2.length();
+            lengths[sz] = 2L * lengths[sz - 1] + rep1.length() + rep2.length() + rep3.length();;
             sz++;
         }
+        
+//        println("sz " + sz + " len " + Arrays.toString(lengths));
         
         int Q = nextInt();
         while(Q-->0) {
@@ -29,9 +31,39 @@ public class Nephrengivesariddle {
                 k -= rep1.length();
                 n--;
             }
-            
-            if(k <= rep1.length())
+//            println("n " + n + " k " + k);
+            if(n >= sz && k <= rep1.length())
                 print(rep1.charAt((int)(k - 1)));
+            else if (n < sz && k > lengths[n])
+                print('.');
+            else {
+                while (true) {
+                    if(n == 0) {
+                        print(base.charAt((int)(k - 1)));
+                        break;
+                    }
+                    else if(k <= rep1.length()) {
+                        print(rep1.charAt((int)(k - 1)));
+                        break;
+                    }
+                    else if(k <= rep1.length() + lengths[n - 1]) {
+                        k -= rep1.length();
+                        n--;
+                    }
+                    else if(k <= rep1.length() + lengths[n - 1] + rep2.length()) {
+                        print(rep2.charAt((int)(k - rep1.length() - lengths[n - 1] - 1)));
+                        break;
+                    }
+                    else if (k <= rep1.length() + 2L * lengths[n - 1] + rep2.length()) {
+                        k -= rep1.length() + lengths[n - 1] + rep2.length();
+                        n--;
+                    }
+                    else {
+                        print(rep3.charAt((int)(k - (rep1.length() + 2L * lengths[n - 1] + rep2.length()) - 1)));    
+                        break;
+                    }
+                }
+            }
             
         }
     }
