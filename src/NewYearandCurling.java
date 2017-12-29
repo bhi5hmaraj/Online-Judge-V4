3aaa416772e8152d1c968c767b407558ffdc319d
bhishmaraj <matib275@gmail.com>
2017-12-30 01:30:01 +0530

    commit at 30/12/2017 01:30:01 IST

--- src/NewYearandCurling.java
+++ src/NewYearandCurling.java
@@ -6,12 +6,6 @@ public class NewYearandCurling {
     
     /************************ SOLUTION STARTS HERE ************************/
     
-    static final double EPS = 1e-8;
-    static int compare(double a , double b) {
-        return a <= b - EPS ? -1 : a >= b + EPS ? 1 : 0;
-    }
-    
-    
     private static void solve() {
         
         
@@ -20,21 +14,11 @@ public class NewYearandCurling {
         int x[] = nextIntArray(n);
         
         double y[] = new double[n];
-        y[0] = r;
-
-        for(int i = 1; i < n; i++) {
-            int touchPt = -1;
-            double y1 = 0;
-            for(int j = 0; j < i; j++)
-                if(!(x[j] + r < x[i] - r || x[j] - r > x[i] + r) && compare(y[j], y1) > 0) {
-                    y1 = y[j];
-                    touchPt = j;
-                }
-            
-            if(touchPt < 0)
-                y[i] = r;
-            else
-                y[i] = y1 + Math.sqrt(4.0 * r * r - Math.pow(x[i] - x[touchPt], 2));
+        for(int i = 0; i < n; i++) {
+            y[i] = r;
+            for(int j = 0; j < i; j++) 
+                if(!(x[j] + r < x[i] - r || x[j] - r > x[i] + r))
+                    y[i] = Math.max(y[i] , (y[j] + Math.sqrt(4.0 * r * r - Math.pow(x[i] - x[j], 2))));
                     
         }
             
