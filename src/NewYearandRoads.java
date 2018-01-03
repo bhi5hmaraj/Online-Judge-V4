60e4e1510ab0df1f625dc3e0410ba8650d609c62
bhishmaraj <matib275@gmail.com>
2018-01-03 17:30:01 +0530

    commit at 03/01/2018 17:30:01 IST

--- src/NewYearandRoads.java
+++ src/NewYearandRoads.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 import java.io.*;
 public class NewYearandRoads {
     
@@ -6,12 +7,21 @@ public class NewYearandRoads {
     
     /************************ SOLUTION STARTS HERE ************************/
     
+    static int maxGap(ArrayList<Integer> arl) {
+        int max = 0;
+        for(int i = 0; i < arl.size() - 1; i++)
+            max = Math.max(max , arl.get(i + 1) - arl.get(i));
+        
+        return max;
+    }
     
     private static void solve() {
         
         ArrayList<ArrayList<Integer>> reds = new ArrayList<>();
         ArrayList<ArrayList<Integer>> blues = new ArrayList<>();
-
+        
+        ArrayList<Integer> greens = new ArrayList<>();
+        
         int minG = Integer.MAX_VALUE;
         int maxG = Integer.MIN_VALUE;
         
@@ -26,10 +36,9 @@ public class NewYearandRoads {
             case 'G':
                 minG = Math.min(minG , p);
                 maxG = Math.max(maxG , p);
-                tempB.add(p);
-                tempR.add(p);
                 blues.add(tempB);
                 reds.add(tempR);
+                greens.add(p);
                 tempB = new ArrayList<>();
                 tempR = new ArrayList<>();
                 break;
@@ -42,8 +51,55 @@ public class NewYearandRoads {
             }
         }
         
+        blues.add(tempB);
+        reds.add(tempR);
         
+        // println("greens " + greens + " blues " + blues + " reds " + reds);
         
+        if(maxG == Integer.MIN_VALUE) { // G is empty
+            List<int[]> combined = blues.get(0).stream().map(p -> new int[]{p , 1}).collect(Collectors.toList());
+            combined.addAll(reds.get(0).stream().map(p -> new int[]{p , 0}).collect(Collectors.toList()));
+            Collections.sort(combined , (p1 , p2) -> p1[0] - p2[0]);
+            
+            cost += blues.get(0).isEmpty() ? 0 : blues.get(0).get(blues.get(0).size() - 1) - blues.get(0).get(0);
+            cost += reds.get(0).isEmpty() ? 0 : reds.get(0).get(reds.get(0).size() - 1) - reds.get(0).get(0);
+            int minDist = Integer.MAX_VALUE;
+            
+            for(int i = 0; i < combined.size() - 1; i++)
+                if((combined.get(i)[1] ^ combined.get(i + 1)[1]) == 1)
+                    minDist = Math.min(minDist , combined.get(i + 1)[0] - combined.get(i)[0]);
+            
+            cost += minDist == Integer.MAX_VALUE ? 0 : minDist;
+        
+        } else {
+            
+            cost += maxG - minG;
+            
+            // segment --1--- G -- ... --- G ---n---
+            // first segment 
+            cost += blues.get(0).isEmpty() ? 0 : greens.get(0) - blues.get(0).get(0);
+            cost += reds.get(0).isEmpty() ? 0 : greens.get(0) - reds.get(0).get(0);
+            
+            // last segment
+            ArrayList<Integer> lastB = blues.get(blues.size() - 1);
+            ArrayList<Integer> lastR = reds.get(reds.size() - 1);
+            cost += lastR.isEmpty() ? 0 : lastR.get(lastR.size() - 1) - greens.get(greens.size() - 1);
+            cost += lastB.isEmpty() ? 0 : lastB.get(lastB.size() - 1) - greens.get(greens.size() - 1);
+            
+            // middle segments
+            for(int i = 1; i < greens.size(); i++) {
+                int prev = greens.get(i - 1);
+                int next = greens.get(i);
+                reds.get(i).add(0, prev);
+                reds.get(i).add(next);
+                blues.get(i).add(0, prev);
+                blues.get(i).add(next);
+                
+                cost += 2L * (next - prev) - maxGap(reds.get(i)) - maxGap(blues.get(i));
+            }
+        }
+        
+        println(cost);
     }
     
     
