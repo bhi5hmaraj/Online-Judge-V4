2b757773b74298cd5f9728dccb7095c435a75c8c
bhishmaraj <matib275@gmail.com>
2018-02-25 13:30:01 +0530

    commit at 25/02/2018 13:30:01 IST

---
+++ src/SUMandREPLACE.java
@@ -0,0 +1,111 @@
+import java.util.*;
+import java.io.*;
+public class SUMandREPLACE {
+    
+    
+    
+    /************************ SOLUTION STARTS HERE ************************/
+    
+    static int tau[];
+    static final int MAX = (int) 1e6;
+    
+    static  {
+        tau = new int[MAX + 1];
+        for(int i = 1; i <= MAX; i++)
+            for(int j = i; j <= MAX; j+= i)
+                tau[j]++;
+    }
+    
+
+    static class FenwickTree {
+
+        /****************
+         * DONT USE BIT IF YOU UPDATE INDEX 0 (causes infinite loop)
+         ******************/
+
+        int tree[];
+        int len;
+
+        FenwickTree(int len) {
+            this.len = len;
+            tree = new int[len + 10];
+        }
+
+        void update(int idx, int val) {
+            if (idx == 0)
+                throw new IndexOutOfBoundsException("BIT IS NOT ZERO INDEXED");
+            for (; idx <= len; idx += (idx & -idx))
+                tree[idx] += val;
+        }
+
+        int query(int idx) {
+            int sum = 0;
+            for (; idx > 0; idx -= (idx & -idx))
+                sum += tree[idx];
+
+            return sum;
+        }
+
+        int query(int L, int R) {
+            return query(R) - query(L - 1);
+        }
+    }
+
+    
+    private static void solve() {
+        
+        
+        int n = nextInt();
+        int m = nextInt();
+        
+        int a[] = nextIntArrayOneBased(n);
+        
+        int ones[] = Arrays.stream(a).map(val -> val == 1 ? 1 : 0).toArray();
+        for(int i = 1; i <= n; i++)
+            ones[i] += ones[i - 1];
+        
+        
+        
+    }
+    
+    
+    
+    /************************ SOLUTION ENDS HERE ************************/
+    
+    
+    
+    
+    
+    /************************ TEMPLATE STARTS HERE **********************/
+    
+    public static void main(String[] args) throws IOException {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)), false);
+        st     = null;
+        solve();
+        reader.close();
+        writer.close();
+    }
+    
+    static BufferedReader reader;
+    static PrintWriter    writer;
+    static StringTokenizer st;
+    
+    static String next()
+    {while(st == null || !st.hasMoreTokens()){try{String line = reader.readLine();if(line == null){return null;}            
+    st = new StringTokenizer(line);}catch (Exception e){throw new RuntimeException();}}return st.nextToken();}
+    static String nextLine()  {String s=null;try{s=reader.readLine();}catch(IOException e){e.printStackTrace();}return s;}             
+    static int    nextInt()   {return Integer.parseInt(next());}
+    static long   nextLong()  {return Long.parseLong(next());}     
+    static double nextDouble(){return Double.parseDouble(next());}
+    static char   nextChar()  {return next().charAt(0);}
+    static int[]  nextIntArray(int n)         {int[] a= new int[n];   int i=0;while(i<n){a[i++]=nextInt();}  return a;}
+    static long[] nextLongArray(int n)        {long[]a= new long[n];  int i=0;while(i<n){a[i++]=nextLong();} return a;}    
+    static int[]  nextIntArrayOneBased(int n) {int[] a= new int[n+1]; int i=1;while(i<=n){a[i++]=nextInt();} return a;}            
+    static long[] nextLongArrayOneBased(int n){long[]a= new long[n+1];int i=1;while(i<=n){a[i++]=nextLong();}return a;}            
+    static void   print(Object o)  { writer.print(o);  }
+    static void   println(Object o){ writer.println(o);}
+    
+    /************************ TEMPLATE ENDS HERE ************************/
+    
+}
