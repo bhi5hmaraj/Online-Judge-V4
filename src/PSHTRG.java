af17502681e19a7565ac83b986aa08a04c0ee400
bhishmaraj <matib275@gmail.com>
2018-03-04 13:30:01 +0530

    commit at 04/03/2018 13:30:01 IST

--- src/PSHTRG.java
+++ src/PSHTRG.java
@@ -7,7 +7,73 @@ class PSHTRG  {
     /************************ SOLUTION STARTS HERE ************************/
     
     static final int THRESHOLD = 45;
-    
+
+    static class SegmentTree  { // Implemented to store min in a range , point update and range query
+        PriorityQueue<Integer> tree[];
+        int len;
+        int size;
+        SegmentTree(int arr[]) { // arr should be a 1 based array
+            len = arr.length - 1;
+            size = 1 << (32 - Integer.numberOfLeadingZeros(len - 1) + 1);  // ceil(log(len)) + 1
+            tree = new PriorityQueue[size];
+            build(arr, 1, 1, len);
+        }
+        
+        PriorityQueue<Integer> merge(PriorityQueue<Integer> pq1, PriorityQueue<Integer> pq2) {
+            PriorityQueue<Integer> pq3 = new PriorityQueue<>(pq1);
+            for(int val : pq2) {
+                pq3.add(val);
+                if(pq3.size() > THRESHOLD)
+                    pq3.remove();
+            }
+            
+            return pq3;
+        }
+        
+        void update(int node,int idx,int val,int nl,int nr) {
+            if(nl == nr && nl == idx)
+                tree[node] = val;
+            else {
+                int mid = (nl + nr) >> 1;
+                if(idx <= mid)
+                    update(2*node, idx , val ,nl , mid);
+                else
+                    update((2*node) + 1, idx ,val , mid + 1, nr);
+
+                tree[node] = Math.min(tree[2*node],tree[(2 * node) + 1]);
+            }
+        }
+        void update(int idx , int val){
+            update(1, idx, val, 1, len);
+        }
+        int query(int L , int R){
+            return query(1, L, R, 1, len);
+        }
+        int query(int node , int L , int R, int nl, int nr) {
+            int mid = (nl + nr) >> 1;
+            if(nl == L && nr == R)
+                return tree[node];
+            else if(R <= mid)
+                return query(2 * node, L, R, nl, mid);
+            else if(L > mid)
+                return query((2*node)+1, L, R, mid + 1 , nr);
+            else
+                return Math.min(query(2*node, L, mid , nl , mid) ,  query((2*node)+1, mid+1, R , mid+1,nr));
+        }
+        void build(int arr[],int node,int L,int R) {
+            if(L == R) {
+                tree[node] = new PriorityQueue<>(1);
+                
+            }
+            else
+            {
+                int mid = (L + R) >> 1;
+                build(arr, 2 * node, L, mid);
+                build(arr, (2 * node) + 1, mid + 1, R);
+                tree[node] = Math.min(tree[2*node] , tree[(2 * node) + 1]);
+            }
+        }
+    }
     private static void solve() {
         
         int N = nextInt();
