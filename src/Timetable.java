004c3f8ac2456e26d1f00dbf91e8aa75ee725e4b
bhishmaraj <matib275@gmail.com>
2018-03-06 22:30:01 +0530

    commit at 06/03/2018 22:30:01 IST

--- src/Timetable.java
+++ src/Timetable.java
@@ -6,6 +6,23 @@ public class Timetable {
     
     /************************ SOLUTION STARTS HERE ************************/
     
+    static int memo[][];
+    static int minMiss[][];
+    
+    static int rec(int idx, int rem) {
+        // System.out.println("idx " + idx + " rem " + rem);
+        if(idx < 0)
+            return 0;
+        else if(memo[idx][rem] != -1)
+            return memo[idx][rem];
+        else {
+            int min = Integer.MAX_VALUE;
+            for(int i = 0; i <= Math.min(rem , minMiss[idx].length - 1); i++)
+                min = Math.min(min , minMiss[idx][i] + rec(idx - 1, rem - i));
+            
+            return memo[idx][rem] = min;
+        }
+    }
     
     private static void solve() {
         
@@ -17,8 +34,41 @@ public class Timetable {
         int table[][] = new int[n][];
         for(int i = 0; i < n; i++) {
             table[i] = nextLine().chars().map(Character::getNumericValue).toArray();
+            for(int j = 1; j < m; j++)
+                table[i][j] += table[i][j - 1];
         }
         
+        minMiss = new int[n][]; // minMiss[*][i] =  min days to go if want to miss i days
+        for(int i = 0; i < n; i++) {
+            // System.out.println(Arrays.toString(table[i]));
+            minMiss[i] = new int[table[i][table[i].length - 1] + 1];
+            int map[] = new int[minMiss[i].length];
+            Arrays.fill(map, -1);
+            for(int j = 0; j < table[i].length; j++)
+                map[table[i][j]] = map[table[i][j]] == -1 ? j : map[table[i][j]];
+            
+            int oldLen = minMiss[i].length - 1;
+            // System.out.println("old " + oldLen);
+            if(oldLen > 0)
+                minMiss[i][oldLen - 1] = 1; // just attend one day
+            
+            for(int miss = 0; miss < oldLen - 1; miss++) {
+                int notMiss = oldLen - miss;
+                minMiss[i][miss] = Integer.MAX_VALUE;
+                for(int j = 0; j < m; j++)
+                    if(table[i][j] >= notMiss)
+                        minMiss[i][miss] = Math.min(minMiss[i][miss] , j - map[table[i][j] - notMiss + 1] + 1);
+            }
+        }
+        /*
+        System.out.println("minMiss");
+        for(int i = 0; i < n; i++)
+            System.out.println(i + " " + Arrays.toString(minMiss[i]));
+        */
+        memo = new int[n][k + 1];
+        for(int a[] : memo) Arrays.fill(a, -1);
+        
+        println(rec(n - 1, k));
         
     }
     
