3c401658906283f842ef2bee1f375372e3e31f45
bhishmaraj <matib275@gmail.com>
2018-04-12 22:30:02 +0530

    commit at 12/04/2018 22:30:02 IST

---
+++ src/MergeEquals.java
@@ -0,0 +1,104 @@
+import java.util.*;
+import java.io.*;
+public class MergeEquals {
+    
+    
+    
+    /************************ SOLUTION STARTS HERE ************************/
+    
+    static class Pair implements Comparable<Pair> {
+        
+        int val , index;
+        
+        public Pair(int val, int index) {
+            this.val = val;
+            this.index = index;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            return val != o.val ? val - o.val : index - o.index;
+        }
+    }
+    
+    private static void solve() {
+        
+        int n = nextInt();
+        int arr[] = nextIntArray(n);
+        
+        HashMap<Integer, PriorityQueue<Pair>> map = new HashMap<>();
+        
+        for(int i = 0; i < n; i++) {
+            int temp = arr[i];
+            while(temp % 2 == 0)
+                temp >>= 1;
+            
+            PriorityQueue<Pair> pq = map.getOrDefault(temp, new PriorityQueue<>());
+            pq.add(new Pair(arr[i], i));
+            map.put(temp, pq);
+        }
+        
+        ArrayList<Pair> collect = new ArrayList<>();
+        
+        map.forEach((k, pq) -> {
+            
+            while(pq.size() > 1) {
+                Pair first = pq.remove();
+                Pair second = pq.remove();
+                if(first.val == second.val)
+                    second.val <<= 1;
+                else
+                    collect.add(first);
+                pq.add(second);
+            }
+            
+            collect.add(pq.remove());
+            
+        });
+        
+        Collections.sort(collect, (p1, p2) -> p1.index - p2.index);
+        println(collect.size());
+        collect.stream().forEach(p -> print(p.val + " "));
+    }
+    
+    
+    
+    /************************ SOLUTION ENDS HERE ************************/
+    
+    
+    
+    
+    
+    /************************ TEMPLATE STARTS HERE **********************/
+    
+    public static void main(String[] args) throws IOException {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)), false);
+        st     = null;
+        solve();
+        reader.close();
+        writer.close();
+    }
+    
+    static BufferedReader reader;
+    static PrintWriter    writer;
+    static StringTokenizer st;
+    
+    static String next()
+    {while(st == null || !st.hasMoreTokens()){try{String line = reader.readLine();if(line == null){return null;}            
+    st = new StringTokenizer(line);}catch (Exception e){throw new RuntimeException();}}return st.nextToken();}
+    static String nextLine()  {String s=null;try{s=reader.readLine();}catch(IOException e){e.printStackTrace();}return s;}             
+    static int    nextInt()   {return Integer.parseInt(next());}
+    static long   nextLong()  {return Long.parseLong(next());}     
+    static double nextDouble(){return Double.parseDouble(next());}
+    static char   nextChar()  {return next().charAt(0);}
+    static int[]  nextIntArray(int n)         {int[] a= new int[n];   int i=0;while(i<n){a[i++]=nextInt();}  return a;}
+    static long[] nextLongArray(int n)        {long[]a= new long[n];  int i=0;while(i<n){a[i++]=nextLong();} return a;}    
+    static int[]  nextIntArrayOneBased(int n) {int[] a= new int[n+1]; int i=1;while(i<=n){a[i++]=nextInt();} return a;}            
+    static long[] nextLongArrayOneBased(int n){long[]a= new long[n+1];int i=1;while(i<=n){a[i++]=nextLong();}return a;}            
+    static void   print(Object o)  { writer.print(o);  }
+    static void   println(Object o){ writer.println(o);}
+    
+    /************************ TEMPLATE ENDS HERE ************************/
+    
+}
